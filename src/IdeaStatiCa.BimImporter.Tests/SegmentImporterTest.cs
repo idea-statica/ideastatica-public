edb70f8a2207245e29443283e74d6d81500cf419
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2021-03-10 18:29:51 +0100

    #2532: added tests for segmentimporter

--- src/IdeaStatiCa.BimImporter.Tests/SegmentImporterTest.cs
+++ src/IdeaStatiCa.BimImporter.Tests/SegmentImporterTest.cs
@@ -1,4 +1,5 @@
-﻿using IdeaRS.OpenModel.Geometry3D;
+﻿using IdeaRS.OpenModel;
+using IdeaRS.OpenModel.Geometry3D;
 using IdeaStatiCa.BimApi;
 using IdeaStatiCa.BimImporter.Importers;
 using NSubstitute;
@@ -19,16 +20,35 @@ namespace IdeaStatiCa.BimImporter.Tests
 			return node;
 		}
 
+		private static Vector3D CreateVector(double x, double y, double z)
+		{
+			return new Vector3D()
+			{
+				X = x,
+				Y = y,
+				Z = z,
+			};
+		}
+
+		//----------
+
 		private static IIdeaLineSegment3D CreateMockLineSegment()
 		{
 			IIdeaNode node1 = CreateMockNode("node1", 0, 0, 0);
 			IIdeaNode node2 = CreateMockNode("node2", 1, 1, 1);
 
+			CoordSystemByVector coordSystem = new CoordSystemByVector()
+			{
+				VecX = CreateVector(1, 0, 0),
+				VecY = CreateVector(0, 1, 0),
+				VecZ = CreateVector(0, 0, 1)
+			};
+
 			IIdeaLineSegment3D segment = Substitute.For<IIdeaLineSegment3D>();
 			segment.Id.Returns("1");
 			segment.StartNode.Returns(node1);
 			segment.EndNode.Returns(node2);
-			segment.LocalCoordinateSystem.Returns(new CoordSystemByVector());
+			segment.LocalCoordinateSystem.Returns(coordSystem);
 			return segment;
 		}
 
@@ -45,6 +65,108 @@ namespace IdeaStatiCa.BimImporter.Tests
 
 			// Tested method
 			Assert.That(() => segmentImporter.Import(new ImportContext(), segment), Throws.TypeOf<ConstraintException>());
+		}
+
+		[Test]
+		public void SegmentImport_WhenLCSIsNotInstanceOfCoordSystemByVector_ThrowsException()
+		{
+			// Setup - set LocalCoordinateSystem to something else than CoordSystemByVector (CoordSystemByZup here)
+			IIdeaLineSegment3D segment = CreateMockLineSegment();
+			segment.LocalCoordinateSystem.Returns(new CoordSystemByZup());
+
+			SegmentImporter segmentImporter = new SegmentImporter(null);
+
+			// Tested method
+			Assert.That(() => segmentImporter.Import(new ImportContext(), segment), Throws.TypeOf<ConstraintException>());
+		}
+
+		[Test]
+		public void SegmentImport_WhenSegmentIsNotLineOrArcSegment_ThrowsException()
+		{
+			// Setup
+			IIdeaNode node1 = CreateMockNode("node1", 0, 0, 0);
+			IIdeaNode node2 = CreateMockNode("node2", 1, 1, 1);
+
+			IIdeaSegment3D segment = Substitute.For<IIdeaSegment3D>();
+			segment.Id.Returns("1");
+			segment.StartNode.Returns(node1);
+			segment.EndNode.Returns(node2);
+			segment.LocalCoordinateSystem.Returns(new CoordSystemByVector());
+
+			SegmentImporter segmentImporter = new SegmentImporter(null);
+
+			// Tested method
+			Assert.That(() => segmentImporter.Import(new ImportContext(), segment), Throws.TypeOf<ConstraintException>());
+		}
+
+		[TestCase(new double[] { 2, 0, 0 }, new double[] { 0, 1, 0 }, new double[] { 0, 0, 1 })]
+		[TestCase(new double[] { 1, 0, 0 }, new double[] { 0, 2, 0 }, new double[] { 0, 0, 1 })]
+		[TestCase(new double[] { 1, 0, 0 }, new double[] { 0, 1, 0 }, new double[] { 0, 0, 2 })]
+		public void SegmentImport_WhenLCSBasisVectorAreNotUnitVectors_ThrowsException(double[] vecX, double[] vecY, double[] vecZ)
+		{
+			// Setup
+			CoordSystemByVector coordSystem = new CoordSystemByVector()
+			{
+				VecX = CreateVector(vecX[0], vecX[1], vecX[2]),
+				VecY = CreateVector(vecY[0], vecY[1], vecY[2]),
+				VecZ = CreateVector(vecZ[0], vecZ[1], vecZ[2])
+			};
+
+			IIdeaLineSegment3D segment = CreateMockLineSegment();
+			segment.LocalCoordinateSystem.Returns(coordSystem);
+
+			SegmentImporter segmentImporter = new SegmentImporter(Substitute.For<IImporter<IIdeaNode>>());
+
+			// Tested method
+			Assert.That(() => segmentImporter.Import(new ImportContext(), segment), Throws.TypeOf<ConstraintException>());
+		}
+
+		[Test]
+		public void SegmentImport_ShouldNormalizeLCSVectors()
+		{
+			// Setup
+			CoordSystemByVector coordSystem = new CoordSystemByVector()
+			{
+				VecX = CreateVector(1 + double.Epsilon, 0 + double.Epsilon, 0 - double.Epsilon),
+				VecY = CreateVector(0 + double.Epsilon, 1 - double.Epsilon, 0 - double.Epsilon),
+				VecZ = CreateVector(0 - double.Epsilon, 0 + double.Epsilon, 1 + double.Epsilon)
+			};
+
+			IIdeaLineSegment3D segment = CreateMockLineSegment();
+			segment.LocalCoordinateSystem.Returns(coordSystem);
+
+			SegmentImporter segmentImporter = new SegmentImporter(Substitute.For<IImporter<IIdeaNode>>());
+
+			// Tested method
+			ReferenceElement refElm = segmentImporter.Import(new ImportContext(), segment);
+			CoordSystemByVector lcs = ((refElm.Element as Segment3D).LocalCoordinateSystem as CoordSystemByVector);
+
+			// Assert
+			Assert.That(lcs.VecX.X, Is.EqualTo(1));
+			Assert.That(lcs.VecX.Y, Is.EqualTo(0));
+			Assert.That(lcs.VecX.Z, Is.EqualTo(0));
+
+			Assert.That(lcs.VecY.X, Is.EqualTo(0));
+			Assert.That(lcs.VecY.Y, Is.EqualTo(1));
+			Assert.That(lcs.VecY.Z, Is.EqualTo(0));
+
+			Assert.That(lcs.VecZ.X, Is.EqualTo(0));
+			Assert.That(lcs.VecZ.Y, Is.EqualTo(0));
+			Assert.That(lcs.VecZ.Z, Is.EqualTo(1));
+		}
+
+		public void SegmentImport_ShouldAddTheLineSegmentIntoOpenModel()
+		{
+			// Setup
+			IIdeaLineSegment3D segment = CreateMockLineSegment();
+			SegmentImporter segmentImporter = new SegmentImporter(null);
+			ImportContext ctx = new ImportContext();
+
+			// Tested method
+			ReferenceElement refElm = segmentImporter.Import(ctx, segment);
+
+			// Assert
+			ctx.OpenModel.LineSegment3D.Contains(refElm.Element as LineSegment3D);
 		}
 	}
 }
--- src/IdeaStatiCa.BimImporter/AssemblyInfo.cs
+++ src/IdeaStatiCa.BimImporter/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("IdeaStatiCa.BimImporter.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=00240000048000009400000006020000002400005253413100040000010001" +
-	"00c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717" +
-	"241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
