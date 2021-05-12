d59e4e508aff87de1f333a2d39a8e5b529bab261
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2021-05-12 19:19:27 +0200

    #2832: added importing by name

--- src/IdeaStatiCa.BimApi/IIdeaMaterialByName.cs
+++ src/IdeaStatiCa.BimApi/IIdeaMaterialByName.cs
@@ -1,9 +1,13 @@
 namespace IdeaStatiCa.BimApi
 {
-    /// <summary>
-    /// A material defined by <see cref="IIdeaObject.Name"/>.
-    /// </summary>
-    public interface IIdeaMaterialByName : IIdeaMaterial
-    {
-    }
+	/// <summary>
+	/// A material defined by <see cref="IIdeaObject.Name"/>.
+	/// </summary>
+	public interface IIdeaMaterialByName : IIdeaMaterial
+	{
+		/// <summary>
+		/// Material type
+		/// </summary>
+		MaterialType MaterialType { get; }
+	}
 }
--- src/IdeaStatiCa.BimImporter/Importers/MaterialImporter.cs
+++ src/IdeaStatiCa.BimImporter/Importers/MaterialImporter.cs
@@ -14,7 +14,18 @@ namespace IdeaStatiCa.BimImporter.Importers
 
 		protected override OpenElementId ImportInternal(IImportContext ctx, IIdeaMaterial material)
 		{
-			if (material is IIdeaMaterialSteel matSteal)
+			if (material is IIdeaMaterialByName materialByName)
+			{
+				if(material.Name is null)
+				{
+					throw new ConstraintException("Name property must not be null for IIdeaMaterialByName.");
+				}
+
+				Material mat = CreateMaterialFromType(materialByName.MaterialType);
+				mat.LoadFromLibrary = true;
+				mat.Name = material.Name;
+			}
+			else if (material is IIdeaMaterialSteel matSteal)
 			{
 				MatSteel mat = matSteal.Material;
 
@@ -24,6 +35,35 @@ namespace IdeaStatiCa.BimImporter.Importers
 				}
 
 				return mat;
+			}
+			else if (material is IIdeaMaterialConcrete matConcrete)
+			{
+				MatConcrete mat = matConcrete.Material;
+
+				if (mat.Name == null)
+				{
+					mat.Name = material.Name;
+				}
+
+				return mat;
+			}
+
+			throw new NotImplementedException();
+		}
+
+		private Material CreateMaterialFromType(MaterialType matType)
+		{
+			// we use ECEN materials just as placeholders
+			switch (matType)
+			{
+				case MaterialType.Concrete:
+					return new MatConcreteEc2();
+
+				case MaterialType.Reinforcement:
+					return new MatReinforcementEc2();
+
+				case MaterialType.Steal:
+					return new MatSteelEc2();
 			}
 
 			throw new NotImplementedException();
