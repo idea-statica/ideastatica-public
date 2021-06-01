c87275e924ca9baef984675b9d52425f5d9626d2
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-06-01 14:39:30 +0200

    #4700 - adding DeleteAllOperationsCommand

---
+++ src/Examples/ConnHiddenCalc/IdeaStatiCa.ConnectionClient/ConHiddenCalcCommands/DeleteAllOperationsCommand.cs
@@ -0,0 +1,24 @@
+﻿using IdeaStatiCa.ConnectionClient.Model;
+
+namespace IdeaStatiCa.ConnectionClient.Commands
+{
+	public class DeleteAllOperationsCommand : ConnHiddenCalcCommandBase
+	{
+		public DeleteAllOperationsCommand(IConHiddenCalcModel model) : base(model)
+		{
+		}
+
+		public override bool CanExecute(object parameter)
+		{
+			return (Model.IsIdea && Model.IsService && Model.SelectedConnection != null && !IsCommandRunning);
+		}
+
+		public override void Execute(object parameter)
+		{
+			var service = Model.GetConnectionService();
+			var connection = (IConnectionId)parameter;
+			var res = service.DeleteAllOperations(connection.ConnectionId);
+			Model.SetStatusMessage(res);
+		}
+	}
+}
