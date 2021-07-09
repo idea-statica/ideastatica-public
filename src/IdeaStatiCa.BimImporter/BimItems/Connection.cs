44b21c82f987100300abb84b175ec3581ff2f73a
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2021-07-09 12:30:04 +0200

    Added implementation of IBimItem

---
+++ src/IdeaStatiCa.BimImporter/BimItems/Connection.cs
@@ -0,0 +1,18 @@
+﻿using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimImporter.ImportedObjects;
+using IdeaStatiCa.Plugin;
+
+namespace IdeaStatiCa.BimImporter.BimItems
+{
+	internal class Connection : IBimItem
+	{
+		public BIMItemType Type => BIMItemType.Node;
+
+		public IIdeaObject ReferencedObject { get; }
+
+		public Connection(ConnectionPoint connection)
+		{
+			ReferencedObject = connection;
+		}
+	}
+}
---
+++ src/IdeaStatiCa.BimImporter/BimItems/Member.cs
@@ -0,0 +1,31 @@
+﻿using IdeaRS.OpenModel;
+using IdeaStatiCa.BimApi;
+using IdeaStatiCa.Plugin;
+
+namespace IdeaStatiCa.BimImporter.BimItems
+{
+	internal class Member : IBimItem
+	{
+		public BIMItemType Type => BIMItemType.Member;
+
+		public IIdeaObject ReferencedObject { get; }
+
+		private readonly IIdeaMember1D _member;
+
+		public Member(IIdeaMember1D member)
+		{
+			_member = member;
+		}
+
+		public BIMItemId Create(IImportContext ctx)
+		{
+			ReferenceElement refElm = ctx.Import(_member);
+
+			return new BIMItemId()
+			{
+				Id = refElm.Id,
+				Type = BIMItemType.Member
+			};
+		}
+	}
+}
