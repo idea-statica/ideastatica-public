8bcad5776000f5fd3bb4c00da571df4f91e68b5e
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2021-08-10 12:35:58 +0200

    Doc comments

--- src/IdeaStatiCa.BimApi/IIdeaCrossSectionByName.cs
+++ src/IdeaStatiCa.BimApi/IIdeaCrossSectionByName.cs
@@ -1,5 +1,10 @@
 ﻿namespace IdeaStatiCa.BimApi
 {
+	/// <summary>
+	/// A cross-section to import by name, any name is allowed. 
+	/// The correct cross-section is resolved by the user in CCM/Checkbot.
+	/// <see cref="IIdeaObject.Name"/> must not be null.
+	/// </summary>
 	public interface IIdeaCrossSectionByName : IIdeaCrossSection
 	{
 		/// <summary>
