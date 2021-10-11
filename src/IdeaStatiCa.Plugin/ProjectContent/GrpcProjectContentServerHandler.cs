53bcd9f8f496805267d64d327732f9ac6a09a0ac
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-10-11 09:15:49 +0200

    adding GrpcProjectContentServerHandler

---
+++ src/IdeaStatiCa.Plugin/ProjectContent/GrpcProjectContentServerHandler.cs
@@ -0,0 +1,32 @@
+﻿using IdeaStatiCa.Plugin.Grpc;
+using IdeaStatiCa.Public;
+using System;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.Plugin.ProjectContent
+{
+	/// <summary>
+	/// Responsible for accessing to data of a project
+	/// </summary>
+	public class GrpcProjectContentServerHandler : IGrpcMessageHandler
+	{
+		private IProjectContent projectContent;
+
+		public GrpcProjectContentServerHandler()
+		{
+
+		}
+
+		public IProjectContent ProjectContent { get => projectContent; private set => projectContent = value; }
+
+		public Task<object> HandleClientMessage(GrpcMessage message, GrpcClient client)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<object> HandleServerMessage(GrpcMessage message, GrpcServer server)
+		{
+			return Task.FromResult<object>(new object());
+		}
+	}
+}
