6054c38a49a25b4dc5b19df160169d8bf5ac9c14
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-10-13 17:30:27 +0200

    adding GrpcServerHost

---
+++ src/SystemTests/GrpcServerHost/Program.cs
@@ -0,0 +1,35 @@
+﻿using IdeaStatiCa.Plugin.Grpc.Reflection;
+using IdeaStatiCa.Plugin.Utilities;
+using System;
+using SystemTestService;
+
+namespace GrpcServerHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int grpcPort = 0;
+			if (args.Length > 0)
+			{
+				var firstArg = args[0];
+				string portNumber = firstArg.Substring("-port:".Length);
+				grpcPort = int.Parse(portNumber);
+			}
+			else
+			{
+				grpcPort = PortFinder.FindPort(50000, 50500);
+			}
+			
+			var service = new Service();
+
+			// Create Grpc server
+			var grpcServer = new GrpcReflectionServer(service, grpcPort);
+			grpcServer.Start();
+
+			Console.WriteLine($"GrpcServer is listening on port {grpcPort}");
+
+			var l = Console.ReadLine();
+		}
+	}
+}
