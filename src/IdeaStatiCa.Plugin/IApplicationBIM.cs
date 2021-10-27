ccd7feeda31210d9acfa4498ce1261075c366fdc
Lukas Hron <lukas.hron@ideastatica.com>
2021-10-27 15:27:15 +0200

    Remove unnecessary RequestedItemType

--- src/IdeaStatiCa.Plugin/IApplicationBIM.cs
+++ src/IdeaStatiCa.Plugin/IApplicationBIM.cs
@@ -9,9 +9,6 @@ namespace IdeaStatiCa.Plugin
 		Connections,
 		Substructure,
 		SingleConnection,
-
-		// Items, that are not in any design item (connection, substructure), but should be synchronized
-		Unassigned,
 	}
 
 	/// <summary>
@@ -29,6 +26,14 @@ namespace IdeaStatiCa.Plugin
 		[OperationContract]
 		string GetApplicationName();
 
+		/// <summary>
+		/// Returns a xml string representing a list of BIM models for requested sequence of given <paramref name="items"/> - groups.
+		/// Each group in the list of <paramref name="items"/> represents designed item such as connection or member and is defined by the id, type and items belonging to the group.
+		/// The item can also has id = -1, which means, that items (members or nodes typically) in this group doesn't belong to any design item (connection or member).
+		/// </summary>
+		/// <param name="countryCode">The standard that will match the imported models.</param>
+		/// <param name="items">The sequence of items, for which the BIM model is required.</param>
+		/// <returns>A xml string representing a list of BIM models for requested <paramref name="items"/>.</returns>
 		[OperationContract]
 		string GetModelForSelectionXML(IdeaRS.OpenModel.CountryCode countryCode, List<BIMItemsGroup> items);
 
