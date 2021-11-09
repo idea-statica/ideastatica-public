53d0a21683607dc25e62c4012e0aef641077310f
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-11-09 15:12:52 +0100

    merging assembly info files

--- src/bim-links/rstab/IdeaRstabPlugin/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("UT_IdeaRstabFEMPlugin")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("IdeaRstabFEMPluginTestApp")]
--- src/bim-links/rstab/IdeaRstabPlugin/Properties/AssemblyInfo.cs
+++ src/bim-links/rstab/IdeaRstabPlugin/Properties/AssemblyInfo.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCulture("")]
 [assembly: Guid("74300C0C-9804-496C-9A9F-006C66DF1703")]
 [assembly: ComVisible(true)]
 [assembly: CLSCompliant(false)]
+[assembly: InternalsVisibleTo("UT_IdeaRstabFEMPlugin")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: InternalsVisibleTo("IdeaRstabFEMPluginTestApp")]
