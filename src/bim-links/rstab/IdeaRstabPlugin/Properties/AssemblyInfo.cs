a7f8eb1a8731d84d0f87f73593247e818d3e1a66
Martin Pospisil <martin.pospisil@ideastatica.com>
2021-11-09 15:19:01 +0100

    assembly info - removing unused code

--- src/bim-links/rstab/IdeaRstabPlugin/Properties/AssemblyInfo.cs
+++ src/bim-links/rstab/IdeaRstabPlugin/Properties/AssemblyInfo.cs
@@ -7,6 +7,3 @@ using System.Runtime.InteropServices;
 [assembly: Guid("74300C0C-9804-496C-9A9F-006C66DF1703")]
 [assembly: ComVisible(true)]
 [assembly: CLSCompliant(false)]
-[assembly: InternalsVisibleTo("UT_IdeaRstabFEMPlugin")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-[assembly: InternalsVisibleTo("IdeaRstabFEMPluginTestApp")]
