ba9e5a3ffbfd14c9d543a6781a0898d4965aa186
nathanlukeIDEA <93578548+nathanlukeIDEA@users.noreply.github.com>
2021-12-02 12:15:42 +0100

    Start of RAM project PoC

---
+++ src/bim-links/bentley-ram/IdeaStatiCa.RamToIdea/BimApi/RamNode.cs
@@ -0,0 +1,20 @@
+﻿//using Dlubal.RSTAB8;
+//using IdeaRstabPlugin.Providers;
+using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimImporter.Persistence;
+using IdeaStatiCa.Plugin;
+using System;
+
+namespace IdeaStatiCa.RamToIdea
+{
+	internal class RamNode : IIdeaNode
+	{
+		public IdeaVector3D Vector => throw new NotImplementedException();
+
+		public IIdeaPersistenceToken Token => throw new NotImplementedException();
+
+		public string Id => throw new NotImplementedException();
+
+		public string Name => throw new NotImplementedException();
+	}
+}
