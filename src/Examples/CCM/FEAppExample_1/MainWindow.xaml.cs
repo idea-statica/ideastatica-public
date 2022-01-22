5fbfa6c43d7b9d79febb7ee302028ffb18a894b6
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-01-22 20:58:29 +0100

    FEAAppExample - UI changes

--- src/Examples/CCM/FEAppExample_1/MainWindow.xaml.cs
+++ src/Examples/CCM/FEAppExample_1/MainWindow.xaml.cs
@@ -1,29 +1,10 @@
-﻿using System;
-using System.Collections.Specialized;
-using System.Globalization;
+﻿using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Media;
 
 namespace FEAppExample_1
 {
-	[ValueConversion(typeof(bool), typeof(bool))]
-	public class InvertBooleanConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			bool original = (bool)value;
-			return !original;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			bool original = (bool)value;
-			return !original;
-		}
-	}
-
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
