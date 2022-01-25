c9693c45868b33a2b187bd3288443d7eb647b368
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-01-25 08:47:16 +0100

    Added InternalsVisibleTo for tests

--- src/bim-links/bentley-ram/IdeaStatiCa.RamToIdea/Properties/AssemblyInfo.cs
+++ src/bim-links/bentley-ram/IdeaStatiCa.RamToIdea/Properties/AssemblyInfo.cs
@@ -1,11 +1,3 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.CompilerServices;
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec8df878-7ce5-458d-bf3e-fec30768a104")]
+[assembly: InternalsVisibleTo("IdeaStatiCa.RamToIdeaTest")]
