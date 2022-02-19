21de483c56daceebab59bb2e4b2c66e80173c554
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-02-19 08:31:18 +0100

    #7809 assembly resolver

--- src/IdeaStatiCa.Public/Tools/AssemblyResolver.cs
+++ src/IdeaStatiCa.Public/Tools/AssemblyResolver.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace IdeaStatiCa.Public.Tools
@@ -6,19 +7,32 @@ namespace IdeaStatiCa.Public.Tools
 	public static class AssemblyResolver
 	{
 		/// <summary>
-		/// AssemblyResolve for GRPC 
+		/// AssemblyResolve forIDEA StatiCa 
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="args"></param>
 		/// <returns></returns>
 		public static Assembly Domain_AssemblyResolve(object sender, ResolveEventArgs args)
 		{
-			string IdeaDirectory = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			AssemblyName assemblyName = new AssemblyName(args.Name);
 
-			if (args.Name.Contains("System.Runtime.CompilerServices.Unsafe")) //Only missing DLL
+			string directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			string path;
+
+			if (string.IsNullOrEmpty(assemblyName.CultureName))
 			{
-				return Assembly.LoadFrom(System.IO.Path.Combine(IdeaDirectory, "System.Runtime.CompilerServices.Unsafe.dll")); //Resolve our missing DLL
+				path = Path.Combine(directory, assemblyName.Name + ".dll");
 			}
+			else
+			{
+				path = Path.Combine(directory, assemblyName.CultureName, assemblyName.Name + ".dll");
+			}
+
+			if (File.Exists(path))
+			{
+				return Assembly.LoadFrom(path);
+			}
+
 			return null;
 		}
 	}
