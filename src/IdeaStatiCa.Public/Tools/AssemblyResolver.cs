1abf3708a06885eb7c046bd9ff87a60199f25d3b
Martin Pospisil <mpospisil@users.noreply.github.com>
2022-02-22 11:14:39 +0100

    7804 assembly resolver etabs (#122)
    * #7806 Assembly resolver for AxisVM
    
    * #7804  ReplaceAssemblyResolve()
    
    * #7804 - log message
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

--- src/IdeaStatiCa.Public/Tools/AssemblyResolver.cs
+++ src/IdeaStatiCa.Public/Tools/AssemblyResolver.cs
@@ -7,6 +7,31 @@ namespace IdeaStatiCa.Public.Tools
 	public static class AssemblyResolver
 	{
 		/// <summary>
+		/// Because the SAP's assembly resolve handler is not implemented properly (it return non-null value even when it can't load the assembly),
+		/// we have to register our handler before it.
+		/// </summary>
+		public static void ReplaceAssemblyResolve()
+		{
+			// Because the SAP's assembly resolve handler is not implemented properly (it return non-null value even when it can't load the assembly),
+			// we have to register our handler before it.
+
+			// AppDomain.AssemblyResolve internally references _AssemblyResolve (see .NET source code);
+			FieldInfo fieldInfo = typeof(AppDomain).GetField("_AssemblyResolve", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			ResolveEventHandler resolveEvent = (ResolveEventHandler)fieldInfo.GetValue(AppDomain.CurrentDomain); // Original event
+			ResolveEventHandler myResolveEvent = new ResolveEventHandler(Domain_AssemblyResolve); // Our event
+
+			// Add all existing handlers to the our event
+			foreach (Delegate handler in resolveEvent.GetInvocationList())
+			{
+				myResolveEvent += (ResolveEventHandler)handler;
+			}
+
+			// Replace the original event with ours in AppDomain.CurrentDomain
+			fieldInfo.SetValue(AppDomain.CurrentDomain, myResolveEvent);
+		}
+
+		/// <summary>
 		/// AssemblyResolve forIDEA StatiCa 
 		/// </summary>
 		/// <param name="sender"></param>
