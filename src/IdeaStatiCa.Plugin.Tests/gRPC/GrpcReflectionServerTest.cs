c29baf53f7aa781196c6292f7fe3866f444d7a2a
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-03-30 21:55:38 +0200

    fix GrpcReflexionServerTest

--- src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcReflectionServerTest.cs
+++ src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcReflectionServerTest.cs
@@ -6,7 +6,8 @@ using NSubstitute;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+using FluentAssertions;
+using System.Threading;
 
 namespace IdeaStatiCa.Plugin.Tests.gRPC
 {
@@ -19,74 +20,90 @@ namespace IdeaStatiCa.Plugin.Tests.gRPC
 
 	public class GrpcReflectionServerTest
 	{
-		///// <summary>
-		///// Test of invocation of method by GrpcReflectionServer
-		///// </summary>
-		///// <returns></returns>
-		//[Test]
-		//public async Task GrpcServerHandleMessagesTest()
-		//{
-		//	const int ExpectedResult = 11;
+		/// <summary>
+		/// Test of invocation of method by GrpcReflectionServer
+		/// </summary>
+		/// <returns></returns>
+		[Test]
+		public async Task GrpcServerHandleMessagesTest()
+		{
+			const int ExpectedResult = 11;
 
-		//	var serviceMock = Substitute.For<ITestService>();
-		//	serviceMock.MakeSum(5, 6).Returns(11);
+			var serviceMock = Substitute.For<ITestService>();
+			serviceMock.MakeSum(5, 6).Returns(11);
 
-		//	var streamReader = Substitute.For<IAsyncStreamReader<GrpcMessage>>();
-		//	var streamWriter = Substitute.For<IServerStreamWriter<GrpcMessage>>();
-		//	var context = Substitute.For<ServerCallContext>();
+			var streamReader = Substitute.For<IAsyncStreamReader<GrpcMessage>>();
+			var streamWriter = Substitute.For<IServerStreamWriter<GrpcMessage>>();
+			var context = Substitute.For<ServerCallContext>();
 
-		//	var reflexionServer = new GrpcReflectionServer(serviceMock, 80, new NullLogger());
+			var reflexionServer = new GrpcReflectionServer(serviceMock, new NullLogger());
 
-		//	List<GrpcMessage> handledMessages = new List<GrpcMessage>();
+			List<GrpcMessage> handledMessages = new List<GrpcMessage>();
 
-		//	const string messageName = Constants.GRPC_REFLECTION_HANDLER_MESSAGE;
+			const string messageName = Constants.GRPC_REFLECTION_HANDLER_MESSAGE;
+			List<GrpcMessage> inputMessages = new List<GrpcMessage>();
 
-		//	List<GrpcMessage> inputMessages = new List<GrpcMessage>();
+			var msg1Data = new GrpcReflectionInvokeData();
+			msg1Data.MethodName = "MakeSum";
+			var parameters = new List<GrpcReflectionArgument>();
+			const int val1 = 5;
+			const int val2 = 6;
+			parameters.Add(new GrpcReflectionArgument().FromVal(val1));
+			parameters.Add(new GrpcReflectionArgument().FromVal(val2));
+			msg1Data.Parameters = parameters;
 
-		//	var msg1Data = new GrpcReflectionInvokeData();
-		//	msg1Data.MethodName = "MakeSum";
-		//	var parameters = new List<GrpcReflectionArgument>();
-		//	const int val1 = 5;
-		//	const int val2 = 6;
-		//	parameters.Add(new GrpcReflectionArgument().FromVal(val1));
-		//	parameters.Add(new GrpcReflectionArgument().FromVal(val2));
-		//	msg1Data.Parameters = parameters;
+			string dataString = JsonConvert.SerializeObject(msg1Data);
 
-		//	string dataString = JsonConvert.SerializeObject(msg1Data);
+			var msg1 = new GrpcMessage();
+			msg1.ClientId = "client1";
+			msg1.MessageName = messageName;
+			msg1.Data = dataString;
+			inputMessages.Add(msg1);
 
-		//	var msg1 = new GrpcMessage();
-		//	msg1.ClientId = "client1";
-		//	msg1.MessageName = messageName;
-		//	msg1.Data = dataString;
-		//	inputMessages.Add(msg1);
+			var readEnumerator = inputMessages.GetEnumerator();
+			ManualResetEvent mre = new ManualResetEvent(false);
 
-		//	var readEnumerator = inputMessages.GetEnumerator();
+			int readCounter = 0;
+			int writeCounter = 0;
+			streamReader.MoveNext().ReturnsForAnyArgs(t =>
+			{
+				if (readCounter < inputMessages.Count)
+				{
+					readCounter++;
+					return readEnumerator.MoveNext();
+				}
+				else
+				{
+					// wait till the last message is processed
+					mre.WaitOne();
+					return false;
+				}
+			});
 
-		//	streamReader.MoveNext().ReturnsForAnyArgs(t =>
-		//	{
-		//		return readEnumerator.MoveNext();
-		//	});
+			streamReader.Current.ReturnsForAnyArgs(t => readEnumerator.Current);
 
-		//	streamReader.Current.ReturnsForAnyArgs(t => readEnumerator.Current);
+			GrpcMessage writtenMessage = null;
 
-		//	GrpcMessage writtenMessage = null;
+			streamWriter.WriteAsync(default).ReturnsForAnyArgs(t =>
+			{
+				writtenMessage = t[0] as GrpcMessage;
+				writeCounter++;
+				// set event to finish gRPC server loop
+				mre.Set();
+				return Task.CompletedTask;
+			});
 
-		//	streamWriter.WriteAsync(default).ReturnsForAnyArgs(t => {
-		//		writtenMessage = t[0] as GrpcMessage;
-		//		return Task.CompletedTask;
-		//	});
+			await reflexionServer.ConnectAsync(streamReader, streamWriter, context);
 
-		//	await reflexionServer.ConnectAsync(streamReader, streamWriter, context);
+	;			// response message should be written
+			Assert.NotNull(writtenMessage);
 
-		//	// response message should be written
-		//	Assert.NotNull(writtenMessage);
+			// data should be exists
+			Assert.NotNull(writtenMessage.Data);
 
-		//	// data should be exists
-		//	Assert.NotNull(writtenMessage.Data);
-
-		//	// expected value is 11
-		//	int returnedVal = JsonConvert.DeserializeObject<int>(writtenMessage.Data);
-		//	Assert.Equal(ExpectedResult, returnedVal);
-		//}
+			// expected value is 11
+			int returnedVal = JsonConvert.DeserializeObject<int>(writtenMessage.Data);
+			returnedVal.Should().Be(ExpectedResult);
+		}
 	}
 }
