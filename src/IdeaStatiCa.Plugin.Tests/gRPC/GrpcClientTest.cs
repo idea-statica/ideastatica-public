fb0d3d88571aae531357be796bfd6315d99f3d9b
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-03-30 20:43:06 +0200

    GrpcClientTest

--- src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcClientTest.cs
+++ src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcClientTest.cs
@@ -1,67 +1,69 @@
-﻿using IdeaStatiCa.Plugin.Grpc;
+﻿using FluentAssertions;
+using IdeaStatiCa.Plugin.Grpc;
 using NSubstitute;
 using NUnit.Framework;
 using System.Threading.Tasks;
-
 
 namespace IdeaStatiCa.Plugin.Tests.gRPC
 {
 	[TestFixture]
 	public class GrpcClientTest
 	{
-	//	[Test]
-	//	public async Task GrpcClientHandleMessagesTest()
-	//	{
-	//		const string clientId = "client1";
+		[Test]
+		public void GrpcClientHandleMessagesTest()
+		{
+			const string clientId = "client1";
 
-	//		var grpcClient = new GrpcClient(clientId, 80, new NullLogger());
+			var grpcClient = new GrpcClient(new NullLogger());
 
-	//		GrpcMessage handler1Msg = null;
+			GrpcMessage handler1Msg = null;
 
-	//		const string messageName1 = "msg1";
-	//		var handler1 = Substitute.For<IGrpcMessageHandler>();
-	//		handler1.HandleClientMessage(default, default).ReturnsForAnyArgs(t => {
-	//			handler1Msg = t[0] as GrpcMessage;
-	//			return Task.FromResult(new object());
-	//		});
-	//		grpcClient.RegisterHandler(messageName1, handler1);
-
-
-	//		const string messageName2 = "msg2";
-	//		GrpcMessage handler2Msg = null;
-	//		var handler2 = Substitute.For<IGrpcMessageHandler>();
-	//		handler2.HandleClientMessage(default, default).ReturnsForAnyArgs(t => {
-
-	//			handler2Msg = t[0] as GrpcMessage;
-	//			return Task.FromResult(new object());
-	//		});
-	//		grpcClient.RegisterHandler(messageName2, handler2);
+			const string messageName1 = "msg1";
+			var handler1 = Substitute.For<IGrpcMessageHandler>();
+			handler1.HandleClientMessage(default, default).ReturnsForAnyArgs(t =>
+			{
+				handler1Msg = t[0] as GrpcMessage;
+				return Task.FromResult(new object());
+			});
+			grpcClient.RegisterHandler(messageName1, handler1);
 
 
-	//		var msg1 = new GrpcMessage();
-	//		msg1.ClientId = clientId;
-	//		msg1.MessageName = messageName1;
-	//		msg1.MessageType = GrpcMessage.Types.MessageType.Response;
-	//		msg1.Data = "1";
+			const string messageName2 = "msg2";
+			GrpcMessage handler2Msg = null;
+			var handler2 = Substitute.For<IGrpcMessageHandler>();
+			handler2.HandleClientMessage(default, default).ReturnsForAnyArgs(t =>
+			{
 
-	//		await grpcClient.HandleMessageAsync(msg1);
+				handler2Msg = t[0] as GrpcMessage;
+				return Task.FromResult(new object());
+			});
+			grpcClient.RegisterHandler(messageName2, handler2);
 
-	//		Assert.NotNull(handler1Msg);
-	//		Assert.Equal("1", handler1Msg.Data);
-	//		Assert.Null(handler2Msg);
-			
-	//		handler1Msg = null;
 
-	//		var msg2 = new GrpcMessage();
-	//		msg2.ClientId = clientId;
-	//		msg2.MessageName = messageName2;
-	//		msg2.MessageType = GrpcMessage.Types.MessageType.Response;
-	//		msg2.Data = "2";
+			var msg1 = new GrpcMessage();
+			msg1.ClientId = clientId;
+			msg1.MessageName = messageName1;
+			msg1.MessageType = GrpcMessage.Types.MessageType.Response;
+			msg1.Data = "1";
 
-	//		await grpcClient.HandleMessageAsync(msg2);
-	//		Assert.NotNull(handler2Msg);
-	//		Assert.Equal("2", handler2Msg.Data);
-	//		Assert.Null(handler1Msg);
-	//	}
+			grpcClient.HandleMessageAsync(msg1);
+
+			Assert.NotNull(handler1Msg);
+			handler1Msg.Data.Should().Be("1");
+			Assert.Null(handler2Msg);
+
+			handler1Msg = null;
+
+			var msg2 = new GrpcMessage();
+			msg2.ClientId = clientId;
+			msg2.MessageName = messageName2;
+			msg2.MessageType = GrpcMessage.Types.MessageType.Response;
+			msg2.Data = "2";
+
+			grpcClient.HandleMessageAsync(msg2);
+			Assert.NotNull(handler2Msg);
+			handler2Msg.Data.Should().Be("2");
+			Assert.Null(handler1Msg);
+		}
 	}
 }
