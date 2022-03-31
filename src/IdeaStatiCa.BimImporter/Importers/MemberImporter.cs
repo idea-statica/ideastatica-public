c433ea0bd85f258e21061a04e6adc00a49c89122
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-03-31 13:49:12 +0200

    Added import of spans to BimImporter

--- src/IdeaStatiCa.BimImporter/Importers/MemberImporter.cs
+++ src/IdeaStatiCa.BimImporter/Importers/MemberImporter.cs
@@ -3,6 +3,7 @@ using IdeaRS.OpenModel.Model;
 using IdeaStatiCa.BimApi;
 using IdeaStatiCa.Plugin;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IdeaStatiCa.BimImporter.Importers
 {
@@ -19,10 +20,40 @@ namespace IdeaStatiCa.BimImporter.Importers
 				throw new ConstraintException("A member has to specify at least one element.");
 			}
 
-			List<ReferenceElement> refElements = new List<ReferenceElement>();
 			List<IIdeaElement1D> elements = member.Elements;
 
+			IIdeaCrossSection css;
+#pragma warning disable CS0618 // Type or member is obsolete
+			if (!(member.CrossSection is null))
+			{
+				css = member.CrossSection;
+			}
+			else if (!(elements[0].StartCrossSection is null))
+			{
+				Logger.LogInformation("Element cross-section are obsolete, use member cross-section or member spans for haunched members.");
+				css = elements[0].StartCrossSection;
+			}
+			else
+			{
+				throw new ConstraintException("Cross-section cannot be null.");
+			}
+#pragma warning restore CS0618 // Type or member is obsolete
+
+			return new Member1D
+			{
+				Name = member.Name,
+				Member1DType = member.Type,
+				Elements1D = ImportElements(ctx, elements),
+				CrossSection = ctx.Import(css),
+				Spans = ImportSpans(ctx, member.Spans)
+			};
+		}
+
+		private List<ReferenceElement> ImportElements(IImportContext ctx, List<IIdeaElement1D> elements)
+		{
+			List<ReferenceElement> refElements = new List<ReferenceElement>(elements.Count);
 			IIdeaNode prevNode = null;
+
 			for (int i = 0; i < elements.Count; i++)
 			{
 				IIdeaElement1D element = elements[i];
@@ -49,12 +80,14 @@ namespace IdeaStatiCa.BimImporter.Importers
 				refElements.Add(refElement);
 			}
 
-			return new Member1D
-			{
-				Name = member.Name,
-				Member1DType = member.Type,
-				Elements1D = refElements
-			};
+			return refElements;
+		}
+
+		private List<ReferenceElement> ImportSpans(IImportContext ctx, IEnumerable<IIdeaSpan> spans)
+		{
+			return spans
+				.Select(x => ctx.Import(x))
+				.ToList();
 		}
 	}
 }
