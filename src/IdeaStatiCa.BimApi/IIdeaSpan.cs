ec03e1c6a1d18fcd166fb3efc082a6bbc8f7cc3b
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-03-31 10:05:58 +0200

    Added member spans

---
+++ src/IdeaStatiCa.BimApi/IIdeaSpan.cs
@@ -0,0 +1,19 @@
+﻿namespace IdeaStatiCa.BimApi
+{
+	public interface IIdeaSpan
+	{
+		/// <summary>
+		/// Cross-section at the start of the element.
+		/// </summary>
+		IIdeaCrossSection StartCrossSection { get; }
+
+		/// <summary>
+		/// Cross-section at the and of the element.
+		/// </summary>
+		IIdeaCrossSection EndCrossSection { get; }
+
+		double StartPosition { get; }
+
+		double EndPosition { get; }
+	}
+}
