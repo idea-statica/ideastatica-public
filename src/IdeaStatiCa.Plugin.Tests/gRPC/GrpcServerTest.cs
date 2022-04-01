1efa5e5ac7d10f376a11d9cb4b3d041b1452388e
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-04-01 10:28:22 +0200

    removing  GrpcServerTwoHandlesTest

--- src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcServerTest.cs
+++ src/IdeaStatiCa.Plugin.Tests/gRPC/GrpcServerTest.cs
@@ -95,79 +95,79 @@ namespace IdeaStatiCa.Plugin.Tests.gRPC
 			handledMessages.Count.Should().Be(2);
 		}
 
-		/// <summary>
-		/// Test of GrpcServer which has more registered handles
-		/// </summary>
-		/// <returns></returns>
-		[Test]
-		public async Task GrpcServerTwoHandlesTest()
-		{
-			var grpcServer = new GrpcServer(new NullLogger());
+		///// <summary>
+		///// Test of GrpcServer which has more registered handles
+		///// </summary>
+		///// <returns></returns>
+		//[Test]
+		//public async Task GrpcServerTwoHandlesTest()
+		//{
+		//	var grpcServer = new GrpcServer(new NullLogger());
 
-			var streamReader = Substitute.For<IAsyncStreamReader<GrpcMessage>>();
-			var streamWriter = Substitute.For<IServerStreamWriter<GrpcMessage>>();
-			var context = Substitute.For<ServerCallContext>();
+		//	var streamReader = Substitute.For<IAsyncStreamReader<GrpcMessage>>();
+		//	var streamWriter = Substitute.For<IServerStreamWriter<GrpcMessage>>();
+		//	var context = Substitute.For<ServerCallContext>();
 
-			List<GrpcMessage> handledMsgsHandler1 = new List<GrpcMessage>();
-			List<GrpcMessage> handledMsgsHandler2 = new List<GrpcMessage>();
+		//	List<GrpcMessage> handledMsgsHandler1 = new List<GrpcMessage>();
+		//	List<GrpcMessage> handledMsgsHandler2 = new List<GrpcMessage>();
 
-			var handler1 = Substitute.For<IGrpcMessageHandler>();
-			handler1.HandleServerMessage(default, default).ReturnsForAnyArgs(t =>
-			{
-				handledMsgsHandler1.Add(t[0] as GrpcMessage);
-				return Task.FromResult(new object());
-			});
+		//	var handler1 = Substitute.For<IGrpcMessageHandler>();
+		//	handler1.HandleServerMessage(default, default).ReturnsForAnyArgs(t =>
+		//	{
+		//		handledMsgsHandler1.Add(t[0] as GrpcMessage);
+		//		return Task.FromResult(new object());
+		//	});
 
-			const string handler1msgName = "msg1";
-			grpcServer.RegisterHandler(handler1msgName, handler1);
+		//	const string handler1msgName = "msg1";
+		//	grpcServer.RegisterHandler(handler1msgName, handler1);
 
 
-			var handler2 = Substitute.For<IGrpcMessageHandler>();
-			handler2.HandleServerMessage(default, default).ReturnsForAnyArgs(t =>
-			{
-				handledMsgsHandler2.Add(t[0] as GrpcMessage);
-				return Task.FromResult(new object());
-			});
+		//	var handler2 = Substitute.For<IGrpcMessageHandler>();
+		//	handler2.HandleServerMessage(default, default).ReturnsForAnyArgs(t =>
+		//	{
+		//		handledMsgsHandler2.Add(t[0] as GrpcMessage);
+		//		return Task.FromResult(new object());
+		//	});
 
-			const string handler2msgName = "msg2";
-			grpcServer.RegisterHandler(handler2msgName, handler2);
+		//	const string handler2msgName = "msg2";
+		//	grpcServer.RegisterHandler(handler2msgName, handler2);
 
-			// prepare two messages to process
-			List<GrpcMessage> inputMessages = new List<GrpcMessage>();
+		//	// prepare two messages to process
+		//	List<GrpcMessage> inputMessages = new List<GrpcMessage>();
 
-			var msg1 = new GrpcMessage();
-			msg1.ClientId = "client1";
-			msg1.MessageName = handler1msgName;
-			msg1.Data = "1";
-			inputMessages.Add(msg1);
+		//	var msg1 = new GrpcMessage();
+		//	msg1.ClientId = "client1";
+		//	msg1.MessageName = handler1msgName;
+		//	msg1.Data = "1";
+		//	inputMessages.Add(msg1);
 
-			var msg2 = new GrpcMessage();
-			msg2.ClientId = "client1";
-			msg2.MessageName = handler2msgName;
-			msg2.Data = "2";
-			inputMessages.Add(msg2);
+		//	var msg2 = new GrpcMessage();
+		//	msg2.ClientId = "client1";
+		//	msg2.MessageName = handler2msgName;
+		//	msg2.Data = "2";
+		//	inputMessages.Add(msg2);
 
-			var msg3 = new GrpcMessage();
-			msg3.ClientId = "client1";
-			msg3.MessageName = handler2msgName;
-			msg3.Data = "3";
-			inputMessages.Add(msg3);
+		//	var msg3 = new GrpcMessage();
+		//	msg3.ClientId = "client1";
+		//	msg3.MessageName = handler2msgName;
+		//	msg3.Data = "3";
+		//	inputMessages.Add(msg3);
 
-			var readEnumerator = inputMessages.GetEnumerator();
+		//	var readEnumerator = inputMessages.GetEnumerator();
 
-			streamReader.MoveNext().ReturnsForAnyArgs(t =>
-			{
-				return readEnumerator.MoveNext();
-			});
+		//	streamReader.MoveNext().ReturnsForAnyArgs(t =>
+		//	{
+		//		return readEnumerator.MoveNext();
+		//	});
 
-			streamReader.Current.ReturnsForAnyArgs(t => readEnumerator.Current);
+		//	streamReader.Current.ReturnsForAnyArgs(t => readEnumerator.Current);
 
-			await grpcServer.ConnectAsync(streamReader, streamWriter, context);
+		//	await grpcServer.ConnectAsync(streamReader, streamWriter, context);
 
-			// each message handler should be called two times
-			handledMsgsHandler1.Should().ContainSingle();
-			handledMsgsHandler2.Count.Should().Be(2);
-		}
+		//	// each message handler should be called two times
+		//	handledMsgsHandler1.Should().ContainSingle();
+		//	handledMsgsHandler2.Count.Should().Be(2);
+		//}
 
 		protected virtual void Dispose(bool disposing)
 		{
