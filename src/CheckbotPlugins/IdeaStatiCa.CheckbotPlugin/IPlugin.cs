b6d17cd337a15ce79106df5f0afdbe4a3b5d2dc3
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-05-30 00:50:29 +0200

    Added comments.

--- src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/IPlugin.cs
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/IPlugin.cs
@@ -1,22 +1,24 @@
 using IdeaStatiCa.CheckbotPlugin.Models;
+using IdeaStatiCa.CheckbotPlugin.Services;
 using System;
 
 namespace IdeaStatiCa.CheckbotPlugin
 {
 	/// <summary>
-	///
+	/// Checkbot plugin interface. Class implementing this interface must be public.
 	/// </summary>
 	public interface IPlugin
 	{
 		/// <summary>
-		/// Returns
+		/// Returns information about the plugin.
 		/// </summary>
 		PluginInfo PluginInfo { get; }
 
 		/// <summary>
-		///
+		/// Entrypoint method, called when the plugin is started.
 		/// </summary>
-		/// <param name="serviceProvider"></param>
+		/// <param name="serviceProvider">Provider for Checkbot plugin services 
+		/// (e.g. <see cref="IProjectService"/>, <see cref="IKeyValueStorage"/>, ...).</param>
 		void Entrypoint(IServiceProvider serviceProvider);
 	}
 }
--- src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/Services/IApplicationService.cs
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/Services/IApplicationService.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace IdeaStatiCa.CheckbotPlugin.Services
 {
+	/// <summary>
+	/// Provides application settings (units, language, etc.).
+	/// </summary>
 	public interface IApplicationService
 	{
 		string GetSettings(string key);
