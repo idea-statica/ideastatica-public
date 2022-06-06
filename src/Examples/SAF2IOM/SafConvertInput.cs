cc910a96ed84d4584fd835744beab9fd3aa66eaa
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-06-06 12:45:55 +0200

    adding comments

--- src/Examples/SAF2IOM/SafConvertInput.cs
+++ src/Examples/SAF2IOM/SafConvertInput.cs
@@ -5,11 +5,20 @@ using System.IO;
 
 namespace SAF2IOM
 {
+	/// <summary>
+	/// Holds input data for SAF to IOM converter.
+	/// </summary>
 	public class SafConvertInput : ISafConverterInput
 	{
 		private Stream SafStream;
 		private Stream PersistenceStream;
 
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="countryCode">Contry code for generated IOM</param>
+		/// <param name="safStream">SAF data</param>
+		/// <param name="persistenceStream">id mapping persistence (used for updating)</param>
 		public SafConvertInput(CountryCode countryCode, Stream safStream, Stream persistenceStream)
 		{
 			Debug.Assert(persistenceStream != null);
@@ -19,21 +28,39 @@ namespace SAF2IOM
 			this.CountryCode = countryCode;
 		}
 
+		/// <summary>
+		/// Country code for materials
+		/// </summary>
 		public CountryCode CountryCode { get; private set; }
 
+		/// <summary>
+		/// Returns stream of id mapping persistence. The stream must be closed by the caller.
+		/// Must never return null.
+		/// </summary>
+		/// <param name="writable">Whether the stream should be writtable.</param>
+		/// <returns>Id mapping persistence stream.</returns>
 		public Stream GetPersistenceStream(bool writable = false)
 		{
 			return PersistenceStream;
 		}
 
+		/// <summary>
+		/// Returns stream of SAF. The stream must be closed by the caller.
+		/// </summary>
+		/// <returns>SAF stream</returns>
 		public Stream GetSafStream()
 		{
 			return SafStream;
 		}
 
+		/// <summary>
+		/// Returns whether any store id mapping exists. Generally, it should return true if GetPersistenceStream(writable=true)
+		/// was called before.
+		/// </summary>
+		/// <returns>True if stored id mapping exist, otherwise false.</returns>
 		public bool PersistenceExists()
 		{
-			return true;
+			return false;
 		}
 	}
 }
