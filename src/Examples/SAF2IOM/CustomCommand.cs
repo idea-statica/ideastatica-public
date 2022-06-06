4cc146c3126d144792a2377adeeb6c80a062c985
Martin Pospisil <mpospisil@users.noreply.github.com>
2022-06-06 13:47:02 +0200

    Saf to iom example (#164)
    * adding example SAF2IOM
    
    * adding saf nuget
    
    * UI
    
    * Saf to IOM example
    
    * example SAF 2 IOM
    
    * fix
    
    * adding comments
    
    * example - writing iom file
    
    * removing unused project configurations
    
    * building saf2iom in github action
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

---
+++ src/Examples/SAF2IOM/CustomCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace SAF2IOM
+{
+	public class CustomCommand : ICommand
+	{
+		private Action<object> ExecuteFunc;
+		private Func<object, bool> CanExecuteFunc;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add
+			{
+				CommandManager.RequerySuggested += value;
+			}
+			remove
+			{
+				CommandManager.RequerySuggested -= value;
+			}
+		}
+
+		public CustomCommand(Func<object, bool> canExecuteFunc, Action<object> executeFunc)
+		{
+			Debug.Assert(executeFunc != null);
+			Debug.Assert(canExecuteFunc != null);
+
+			ExecuteFunc = executeFunc;
+			CanExecuteFunc = canExecuteFunc;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return CanExecuteFunc(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			ExecuteFunc(parameter);
+		}
+	}
+}
---
+++ src/Examples/SAF2IOM/SafConvertInput.cs
@@ -0,0 +1,66 @@
+﻿using IdeaRS.OpenModel;
+using IdeaStatiCa.SAF2IOM;
+using System.Diagnostics;
+using System.IO;
+
+namespace SAF2IOM
+{
+	/// <summary>
+	/// Holds input data for SAF to IOM converter.
+	/// </summary>
+	public class SafConvertInput : ISafConverterInput
+	{
+		private Stream SafStream;
+		private Stream PersistenceStream;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="countryCode">Contry code for generated IOM</param>
+		/// <param name="safStream">SAF data</param>
+		/// <param name="persistenceStream">id mapping persistence (used for updating)</param>
+		public SafConvertInput(CountryCode countryCode, Stream safStream, Stream persistenceStream)
+		{
+			Debug.Assert(persistenceStream != null);
+			Debug.Assert(safStream != null);
+			this.SafStream = safStream;
+			this.PersistenceStream = persistenceStream;
+			this.CountryCode = countryCode;
+		}
+
+		/// <summary>
+		/// Country code for materials
+		/// </summary>
+		public CountryCode CountryCode { get; private set; }
+
+		/// <summary>
+		/// Returns stream of id mapping persistence. The stream must be closed by the caller.
+		/// Must never return null.
+		/// </summary>
+		/// <param name="writable">Whether the stream should be writtable.</param>
+		/// <returns>Id mapping persistence stream.</returns>
+		public Stream GetPersistenceStream(bool writable = false)
+		{
+			return PersistenceStream;
+		}
+
+		/// <summary>
+		/// Returns stream of SAF. The stream must be closed by the caller.
+		/// </summary>
+		/// <returns>SAF stream</returns>
+		public Stream GetSafStream()
+		{
+			return SafStream;
+		}
+
+		/// <summary>
+		/// Returns whether any store id mapping exists. Generally, it should return true if GetPersistenceStream(writable=true)
+		/// was called before.
+		/// </summary>
+		/// <returns>True if stored id mapping exist, otherwise false.</returns>
+		public bool PersistenceExists()
+		{
+			return false;
+		}
+	}
+}
