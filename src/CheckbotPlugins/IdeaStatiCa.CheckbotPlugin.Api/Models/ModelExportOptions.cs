35c3f24d6e1e27b2a061151116f042599791f43a
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-06-13 23:43:33 +0200

    Added default values to ModelExportOptions

--- src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin.Api/Models/ModelExportOptions.cs
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin.Api/Models/ModelExportOptions.cs
@@ -2,14 +2,16 @@
 {
 	public class ModelExportOptions
 	{
+		public static readonly ModelExportOptions Default = new ModelExportOptions();
+
 		/// <summary>
 		/// When <see langword="true"/> <see cref="IdeaRS.OpenModel.OpenModelContainer"/> will contains results.
 		/// </summary>
-		public bool WithResults { get; set; }
+		public bool WithResults { get; set; } = true;
 
 		/// <summary>
 		/// When <see langword="true"/> all cross-section in the IOM will be specified as <see cref="IdeaRS.OpenModel.CrossSection.CrossSectionComponent"/>.
 		/// </summary>
-		public bool AllCrossSectionsAsGeneral { get; set; }
+		public bool AllCrossSectionsAsGeneral { get; set; } = false;
 	}
 }
