8e83bc4f4874012bb715045f104de6be5cdc0725
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-06-13 23:45:07 +0200

    Added conversions

--- src/CheckbotPlugins/IdeaStatiCa.PluginRunner.Lib/Utils/ConvertExtensions.cs
+++ src/CheckbotPlugins/IdeaStatiCa.PluginRunner.Lib/Utils/ConvertExtensions.cs
@@ -5,25 +5,58 @@ namespace IdeaStatiCa.PluginRunner.Utils
 {
 	internal static class ModelObjectExtension
 	{
-		public static Models.ModelObject Convert(this Protos.ModelObject modelObject)
+		public static Models.ModelObject FromProto(this Protos.ModelObject modelObject)
+			=> new(modelObject.Type.FromProto(), modelObject.Id);
+
+		public static Protos.ModelObject ToProto(this Models.ModelObject modelObject)
 		{
-			return new Models.ModelObject(modelObject.Type.Convert(), modelObject.Id);
+			return new()
+			{
+				Id = modelObject.Id,
+				Type = modelObject.Type.ToProto()
+			};
 		}
 	}
 
 	internal static class ModelObjectTypeExtension
 	{
-		public static Models.ModelObjectType Convert(this Protos.ModelObjectType objectType)
+		public static Models.ModelObjectType FromProto(this Protos.ModelObjectType objectType)
 		{
 			return objectType switch
 			{
 				Protos.ModelObjectType.ConnectionPoint => Models.ModelObjectType.ConnectionPoint,
-				Protos.ModelObjectType.Substructure => Models.ModelObjectType.ConnectionPoint,
-				Protos.ModelObjectType.Member => Models.ModelObjectType.ConnectionPoint,
-				Protos.ModelObjectType.Node => Models.ModelObjectType.ConnectionPoint,
-				Protos.ModelObjectType.CrossSection => Models.ModelObjectType.ConnectionPoint,
-				Protos.ModelObjectType.Material => Models.ModelObjectType.ConnectionPoint,
+				Protos.ModelObjectType.Substructure => Models.ModelObjectType.Substructure,
+				Protos.ModelObjectType.Member => Models.ModelObjectType.Member,
+				Protos.ModelObjectType.Node => Models.ModelObjectType.Node,
+				Protos.ModelObjectType.CrossSection => Models.ModelObjectType.CrossSection,
+				Protos.ModelObjectType.Material => Models.ModelObjectType.Material,
 				_ => throw new NotImplementedException(),
+			};
+		}
+
+		public static Protos.ModelObjectType ToProto(this Models.ModelObjectType objectType)
+		{
+			return objectType switch
+			{
+				Models.ModelObjectType.ConnectionPoint => Protos.ModelObjectType.ConnectionPoint,
+				Models.ModelObjectType.Substructure => Protos.ModelObjectType.Substructure,
+				Models.ModelObjectType.Member => Protos.ModelObjectType.Member,
+				Models.ModelObjectType.Node => Protos.ModelObjectType.Node,
+				Models.ModelObjectType.CrossSection => Protos.ModelObjectType.CrossSection,
+				Models.ModelObjectType.Material => Protos.ModelObjectType.Material,
+				_ => throw new NotImplementedException(),
+			};
+		}
+	}
+
+	internal static class ModelExportOptionsExtension
+	{
+		public static Protos.ModelExportOptions ToProto(this Models.ModelExportOptions options)
+		{
+			return new Protos.ModelExportOptions()
+			{
+				AllCrossSectionsAsGeneral = options.AllCrossSectionsAsGeneral,
+				WithResults = options.WithResults
 			};
 		}
 	}
