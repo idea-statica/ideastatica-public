0eb0af0e881de5e0f58f17125a835477f1ad4b27
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2022-08-02 11:58:36 +0200

    Added missing changes after revert

---
+++ src/IdeaStatiCa.BimImporter/Importers/TaperImporter.cs
@@ -0,0 +1,26 @@
+﻿using IdeaRS.OpenModel;
+using IdeaRS.OpenModel.Model;
+using IdeaStatiCa.BimApi;
+using IdeaStatiCa.Plugin;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IdeaStatiCa.BimImporter.Importers
+{
+	internal class TaperImporter : AbstractImporter<IIdeaTaper>
+	{
+		public TaperImporter(IPluginLogger logger) : base(logger)
+		{
+		}
+
+		protected override OpenElementId ImportInternal(IImportContext ctx, IIdeaTaper taper)
+		{
+			return new Taper()
+			{
+				Spans = taper?.Spans
+					.Select(x => ctx.Import(x))
+					.ToList() ?? new List<ReferenceElement>()
+			};
+		}
+	}
+}
