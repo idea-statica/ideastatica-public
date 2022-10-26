9fc8b027ec1d1674265e736ac60ea6df8d1fef47
Martin Pospisil <martin.pospisil@ideastatica.com>
2022-10-26 17:20:50 +0200

    changing ideastatica dir StatiCa 22.0 -> StatiCa 22.1

--- src/Examples/CCM/FEAppExample_1/Properties/Settings.Designer.cs
+++ src/Examples/CCM/FEAppExample_1/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace FEAppExample_1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace FEAppExample_1.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 22.0\\IdeaCheckbot.exe")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 22.1\\IdeaCheckbot.exe")]
         public string IdeaStatiCaDir {
             get {
                 return ((string)(this["IdeaStatiCaDir"]));
