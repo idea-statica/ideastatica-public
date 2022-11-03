63c999e05353f71471b378c951ca91093e523d65
pavelvadura <pavel.vadura@ideastatica.com>
2022-11-03 12:03:45 +0100

    11272 iom container add new function for load and store

--- src/IdeaRS.OpenModel/OpenModelContainer.cs
+++ src/IdeaRS.OpenModel/OpenModelContainer.cs
@@ -1,4 +1,8 @@
 ﻿using IdeaRS.OpenModel.Result;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace IdeaRS.OpenModel
 {
@@ -17,5 +21,79 @@ namespace IdeaRS.OpenModel
 		/// Open Model Result - results of a finite element analysis
 		/// </summary>
 		public OpenModelResult OpenModelResult { get; set; }
+
+		/// <summary>
+		/// Saves content into file as a XML format
+		/// </summary>
+		/// <param name="xmlFileName">XML file name</param>
+		/// <returns>True if succeeded</returns>
+		public bool SaveToXmlFile(string xmlFileName)
+		{
+			// Storing to standard xml file
+			XmlSerializer xs = new XmlSerializer(typeof(OpenModelContainer));
+
+			Stream fs = new FileStream(xmlFileName, FileMode.Create);
+			XmlTextWriter writer = new XmlTextWriter(fs, Encoding.Unicode)
+			{
+				Formatting = Formatting.Indented
+			};
+			// Serialize using the XmlTextWriter.
+			xs.Serialize(writer, this);
+			writer.Close();
+			fs.Close();
+
+			return true;
+		}
+
+		/// <summary>
+		/// Creates the instance of Open Model container from the XML file
+		/// </summary>
+		/// <param name="xmlFileName">XML file name</param>
+		/// <returns>Open Model Container or null</returns>
+		public static OpenModelContainer LoadFromXmlFile(string xmlFileName)
+		{
+			OpenModelContainer openModelContainer = null;
+			using (FileStream fs = new FileStream(xmlFileName, FileMode.Open, FileAccess.Read))
+			{
+				openModelContainer = LoadFromStream(fs);
+			}
+
+			return openModelContainer;
+		}
+
+		/// <summary>
+		/// Creates the instance of Open Model container from the stream
+		/// </summary>
+		/// <param name="xmlFileStream">The input stream</param>
+		/// <returns>The new instance of Open Model Container</returns>
+		public static OpenModelContainer LoadFromStream(Stream xmlFileStream)
+		{
+			XmlReaderSettings xmlSettings = new XmlReaderSettings
+			{
+				CloseInput = false
+			};
+
+			XmlReader reader = XmlReader.Create(xmlFileStream, xmlSettings);
+			XmlSerializer xs = new XmlSerializer(typeof(OpenModelContainer));
+			OpenModelContainer openModelContainer = (xs.Deserialize(reader) as OpenModelContainer);
+			openModelContainer.OpenModel.ReferenceElementsReconstruction();
+
+			return openModelContainer;
+		}
+
+		/// <summary>
+		/// Creates the instance of Open Model container from xml string
+		/// </summary>
+		/// <param name="xmlString">The input string</param>
+		/// <returns>The new instance of Open Model Container</returns>
+		public static OpenModelContainer LoadFromString(string xmlString)
+		{
+			StringReader stringReader = new System.IO.StringReader(xmlString);
+			XmlSerializer serializer = new XmlSerializer(typeof(OpenModelContainer));
+			OpenModelContainer openModelContainer = serializer.Deserialize(stringReader) as OpenModelContainer;
+			openModelContainer.OpenModel.ReferenceElementsReconstruction();
+
+			return openModelContainer;
+		}
 	}
 }
