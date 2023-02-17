bb5b398cdc69b48a0c5b84c7cd553a3ca8daef8f
Martin Pospisil <mpospisil@users.noreply.github.com>
2023-02-17 15:02:00 +0100

    adding DataContract attribute to Region2D classes (#245)
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

--- src/IdeaRS.OpenModel/Geometry/Geometry2D/Region2D.cs
+++ src/IdeaRS.OpenModel/Geometry/Geometry2D/Region2D.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Xml.Serialization;
 
 namespace IdeaRS.OpenModel.Geometry2D
@@ -7,6 +8,7 @@ namespace IdeaRS.OpenModel.Geometry2D
 	/// Represents a region in two-dimensional space included outline (border) and openings.
 	/// </summary>
 	[OpenModelClass("CI.Geometry2D.Region2D,CI.Geometry2D")]
+	[DataContract]
 	public class Region2D : OpenObject
 	{
 		/// <summary>
@@ -20,12 +22,14 @@ namespace IdeaRS.OpenModel.Geometry2D
 		/// <summary>
 		/// Gets or sets the outline curve of Region2D.
 		/// </summary>
+		[DataMember]
 		public PolyLine2D Outline { get; set; }
 
 		/// <summary>
 		/// Gets or sets the list of openings in the Region2D.
 		/// </summary>
 		[XmlElement(IsNullable = true)]
+		[DataMember]
 		public List<PolyLine2D> Openings { get; set; }
 	}
 }
--- src/IdeaRS.OpenModel/Geometry/Geometry2D/Segment2D.cs
+++ src/IdeaRS.OpenModel/Geometry/Geometry2D/Segment2D.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml.Serialization;
+﻿using IdeaRS.OpenModel.Message;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace IdeaRS.OpenModel.Geometry2D
 {
@@ -7,11 +9,15 @@ namespace IdeaRS.OpenModel.Geometry2D
 	/// </summary>
 	[XmlInclude(typeof(LineSegment2D))]
 	[XmlInclude(typeof(ArcSegment2D))]
+	[KnownType(typeof(LineSegment2D))]
+	[KnownType(typeof(ArcSegment2D))]
+	[DataContract]
 	public abstract class Segment2D : OpenObject
 	{
 		/// <summary>
 		/// Gets or sets the endpoint of the segment.
 		/// </summary>
+		[DataMember]
 		public Point2D EndPoint { get; set; }
 	}
 }
