3e1dae167321137de25ab87a7e1643dea8a0448c
Jakub Fojtů <jakub.fojtu@ideastatica.com>
2023-04-11 21:51:35 +0200

    #13181 Added members 2D to the BimApiLink

---
+++ src/IdeaStatiCa.BimApiLink/BimApi/IdeaMember2D.cs
@@ -0,0 +1,27 @@
+﻿using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimApiLink.Identifiers;
+using System.Collections.Generic;
+
+namespace IdeaStatiCa.BimApiLink.BimApi
+{
+	public class IdeaMember2D : AbstractIdeaObject<IIdeaMember2D>, IIdeaMember2D
+	{
+		public virtual IIdeaPersistenceToken Token { get; set; }
+
+		public virtual List<IIdeaElement2D> Elements2D { get; set; }
+
+		public IdeaMember2D(Identifier<IIdeaMember2D> identifer)
+			: base(identifer)
+		{
+			Token = identifer;
+		}
+
+		public IdeaMember2D(int id)
+			: this(new IntIdentifier<IIdeaMember2D>(id))
+		{ }
+
+		public IdeaMember2D(string id)
+			: this(new StringIdentifier<IIdeaMember2D>(id))
+		{ }
+	}
+}
---
+++ src/IdeaStatiCa.BimApiLink/BimApi/IdeaPolyLine3D.cs
@@ -0,0 +1,23 @@
+﻿using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimApiLink.Identifiers;
+using System.Collections.Generic;
+
+namespace IdeaStatiCa.BimApiLink.BimApi
+{
+	public class IdeaPolyLine3D : AbstractIdeaObject<IIdeaPolyLine3D>, IIdeaPolyLine3D
+	{
+		public virtual List<IIdeaSegment3D> Segments { get; set; }
+
+		public IdeaPolyLine3D(Identifier<IIdeaPolyLine3D> identifer)
+			: base(identifer)
+		{ }
+
+		public IdeaPolyLine3D(int id)
+			: this(new IntIdentifier<IIdeaPolyLine3D>(id))
+		{ }
+
+		public IdeaPolyLine3D(string id)
+			: this(new StringIdentifier<IIdeaPolyLine3D>(id))
+		{ }
+	}
+}
