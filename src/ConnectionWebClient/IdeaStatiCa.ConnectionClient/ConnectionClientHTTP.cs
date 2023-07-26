e8cab38a20d7131a966463fa8c89b3f05fa4aeef
Martin Pospisil <mpospisil@users.noreply.github.com>
2023-07-26 03:58:59 +0200

    Con calculator aspnet (#302)
    * #12091 implemented new interface for grpc blob storage
    
    * #12091 changed name of the constant
    
    * #12091 fixed GRPC communication task waiting
    
    * #12091 implemented init in GRPC blobstorage
    
    * using
    
    * #12091 fixed connection and member grpc communication
    
    * #12091 working checkbot connection communication
    
    * #12091 merged main
    
    * #12091 fixed public repo
    
    * #12091 added new clean method to interface
    
    * adding connection client
    
    * Opening project from ConnectionWebClient
    
    * connection VMs
    
    * ConnectionClientHTTP
    
    * displaying output text
    
    * ConnectionClientHTTP
    
    * deleting unused solution
    
    ---------
    
    Co-authored-by: David Sapák <david.sapak@ideastatica.com>
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

---
+++ src/ConnectionWebClient/IdeaStatiCa.ConnectionClient/ConnectionClientHTTP.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using IdeaRS.OpenModel.Connection;
+using Newtonsoft.Json;
+
+namespace IdeaStatiCa.ConnectionClient
+{
+	public class ConnectionClientHTTP : IConnectionClient
+	{
+		private readonly HttpClient httpClient;
+		public const string ConCalculatorVersionAPI = "1";
+
+		private OpenProjectResult Project { get; set; }
+
+		public ConnectionClientHTTP(HttpClient httpClient)
+		{
+			this.httpClient = httpClient;
+		}
+
+		public async Task CloseProjectAsync(CancellationToken cancellationToken)
+		{
+			var response = await httpClient.GetAsync($"api/{ConCalculatorVersionAPI}/project/{Project.OpenProjectId}/close");
+
+			response.EnsureSuccessStatusCode();
+		}
+
+		public async Task<ConProjectInfo> OpenProjectAsync(Stream ideaConProject, CancellationToken cancellationToken)
+		{
+			using (MultipartFormDataContent formData = new MultipartFormDataContent())
+			{
+				using (StreamContent streamContent = new StreamContent(ideaConProject))
+				{
+					// Add the StreamContent as a form field
+					formData.Add(streamContent, "ideaConFile", "connection.ideacon");
+
+					var response = await httpClient.PostAsync($"api/{ConCalculatorVersionAPI}/project/upload", formData, cancellationToken);
+
+					response.EnsureSuccessStatusCode();
+
+					var responseJson = await response.Content.ReadAsStringAsync();
+
+					Project = JsonConvert.DeserializeObject<OpenProjectResult>(responseJson);
+				}
+			}
+			return Project.ProjectInfo;
+		}
+
+		public async Task<ConnectionCheckRes> GetBriefResultsAsync(int connectionId, CancellationToken cancellationToken)
+		{
+			var response = await httpClient.GetAsync($"api/{ConCalculatorVersionAPI}/connection/{Project.OpenProjectId}/{connectionId}/brief-results");
+
+			response.EnsureSuccessStatusCode();
+
+			var responseJson = await response.Content.ReadAsStringAsync();
+
+			var res = JsonConvert.DeserializeObject<ConnectionCheckRes>(responseJson);
+			return res;
+		}
+
+		public async Task<string> GetDetailResultsJsonAsync(int connectionId, CancellationToken cancellationToken)
+		{
+			var response = await httpClient.GetAsync($"api/{ConCalculatorVersionAPI}/connection/{Project.OpenProjectId}/{connectionId}/detail-results");
+
+			response.EnsureSuccessStatusCode();
+
+			var responseJson = await response.Content.ReadAsStringAsync();
+			
+			return responseJson;
+		}
+	}
+}
---
+++ src/ConnectionWebClient/IdeaStatiCa.ConnectionClient/IConnectionClient.cs
@@ -0,0 +1,18 @@
+﻿using IdeaRS.OpenModel.Connection;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.ConnectionClient
+{
+	public interface IConnectionClient
+	{
+		Task<ConProjectInfo> OpenProjectAsync(Stream ideaConProject, CancellationToken cancellationToken);
+
+		Task CloseProjectAsync(CancellationToken cancellationToken);
+
+		Task<ConnectionCheckRes> GetBriefResultsAsync(int connectionId, CancellationToken cancellationToken);
+
+		Task<string> GetDetailResultsJsonAsync(int connectionId, CancellationToken cancellationToken);
+	}
+}
