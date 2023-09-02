9d1b1a9a9a5db119161e44f6f427eb9c49bec796
Martin Pospisil <martin.pospisil@ideastatica.com>
2023-09-02 23:10:54 +0200

    comment

--- src/IdeaStatiCa.Public/IBlobStorageProvider.cs
+++ src/IdeaStatiCa.Public/IBlobStorageProvider.cs
@@ -1,7 +1,11 @@
 ﻿namespace IdeaStatiCa.Public
 {
+	/// <summary>
+	/// Is responsible for providing blob storages according to it names
+	/// </summary>
 	public interface IBlobStorageProvider
 	{
+
 		IBlobStorage GetBlobStorage(string blobStorageName);
 
 		void Commit(string blobStorageName);
