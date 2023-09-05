6b8724816303116d7531f6165a36a73ca79803dd
Martin Pospisil <mpospisil@users.noreply.github.com>
2023-09-05 19:41:31 +0200

    Blob storage provider fix (#319)
    * comment
    
    * BlobStorageProvider - comments
    
    ---------
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

--- src/IdeaStatiCa.Public/IBlobStorageProvider.cs
+++ src/IdeaStatiCa.Public/IBlobStorageProvider.cs
@@ -1,10 +1,26 @@
 ﻿namespace IdeaStatiCa.Public
 {
+	/// <summary>
+	/// It is responsible for providing blob storages according to its name
+	/// </summary>
 	public interface IBlobStorageProvider
 	{
+		/// <summary>
+		/// Provides required blob storage which is defined by its name <paramref name="blobStorageName"/>
+		/// </summary>
+		/// <param name="blobStorageName">The name of the required blob storage</param>
+		/// <returns>The instance on the required blob storage</returns>
 		IBlobStorage GetBlobStorage(string blobStorageName);
 
+		/// <summary>
+		/// Commits all changes in the blobstorage
+		/// </summary>
+		/// <param name="blobStorageName">The name of blob storage to commit</param>
 		void Commit(string blobStorageName);
+
+		/// <summary>
+		/// Invalidates changes in all blob storages
+		/// </summary>
 		void CleanBlobStorages();
 	}
 }
