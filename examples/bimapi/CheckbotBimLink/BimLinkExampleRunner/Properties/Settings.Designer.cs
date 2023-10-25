4f3b36abd75822c1b380b4754cb8de130c7ec8d4
Lukas Hron <lukas.hron@ideastatica.com>
2023-10-25 10:08:09 +0200

    Add CAD/FEA switch to the BimLinkExampleRunner

--- examples/bimapi/CheckbotBimLink/BimLinkExampleRunner/Properties/Settings.Designer.cs
+++ examples/bimapi/CheckbotBimLink/BimLinkExampleRunner/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace BimLinkExampleRunner.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace BimLinkExampleRunner.Properties {
             }
             set {
                 this["CheckbotLocation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("CAD")]
+        public BimLinkExampleRunner.ViewModels.ApplicationType ApplicationType {
+            get {
+                return ((BimLinkExampleRunner.ViewModels.ApplicationType)(this["ApplicationType"]));
+            }
+            set {
+                this["ApplicationType"] = value;
             }
         }
     }
