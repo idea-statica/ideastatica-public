e5bba2795ccabb2516f93728051bea5406ddc943
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2023-11-07 13:01:12 +0100

    Doc comments.

--- src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin.PluginList/Descriptors/ActionButtonDescriptor.cs
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin.PluginList/Descriptors/ActionButtonDescriptor.cs
@@ -2,12 +2,27 @@
 {
 	public sealed class ActionButtonDescriptor
 	{
+		/// <summary>
+		/// Action name.
+		/// </summary>
 		public string Name { get; }
 
+		/// <summary>
+		/// Path to the image or base64 encoded image.
+		/// Supported formats are PNG, JPG, BMP.
+		/// 
+		/// To encode 
+		/// </summary>
 		public string Image { get; }
 
+		/// <summary>
+		/// Text displayed on the button in Checkbot.
+		/// </summary>
 		public string Text { get; }
 
+		/// <summary>
+		/// Tooltip of the button in Checkbot.
+		/// </summary>
 		public string Tooltip { get; }
 
 		public ActionButtonDescriptor(string name, string image, string text, string tooltip)
