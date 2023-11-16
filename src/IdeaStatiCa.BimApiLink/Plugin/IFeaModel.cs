aec24d710524eecba7e9e8fede82f80ee302db9f
Tomas Kohoutek <tomas.kohoutek@ideastatica.com>
2023-11-16 10:49:12 +0100

    #17293 Support selecting nodes & members for FEA model

--- src/IdeaStatiCa.BimApiLink/Plugin/IFeaModel.cs
+++ src/IdeaStatiCa.BimApiLink/Plugin/IFeaModel.cs
@@ -1,7 +1,12 @@
-﻿namespace IdeaStatiCa.BimApiLink.Plugin
+﻿using System.Collections.Generic;
+using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimApiLink.Identifiers;
+
+namespace IdeaStatiCa.BimApiLink.Plugin
 {
 	public interface IFeaModel : IModel
 	{
 		FeaUserSelection GetUserSelection();
+		void SelectUserSelection(IEnumerable<Identifier<IIdeaNode>> nodes, IEnumerable<Identifier<IIdeaMember1D>> members);
 	}
 }
