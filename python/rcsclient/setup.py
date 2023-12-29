1d28c92afbbc45b9ee4306608201794a9f869a92
Martin Pospisil <martin.pospisil@ideastatica.com>
2023-12-29 11:00:37 +0100

    building python package 23.1.3.0515

---
+++ python/rcsclient/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ideastatica_rcs_client",
+    version="23.1.3.0515",
+    packages=find_packages(where="ideastatica_rcs_client"),
+    package_dir={"": "ideastatica_rcs_client"},
+)
