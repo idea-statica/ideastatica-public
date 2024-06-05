cf6c6b3930aac37dc6054772231b5dea8c8bd934
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-06-05 14:24:20 +0200

    fix

--- examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApi/NativeFeaGeometry.cpp
+++ examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApi/NativeFeaGeometry.cpp
@@ -5,7 +5,7 @@ NativeFeaGeometry::NativeFeaGeometry()
 {
 	nodes = std::map<int, NativeFeaNode*>();
 	nodes.insert(std::make_pair(1, new NativeFeaNode(1, 0.0, 0.0, 0.0)));
-	nodes.insert(std::make_pair(2, new NativeFeaNode(2, 1.0, 0.0, 3.0)));
+	nodes.insert(std::make_pair(2, new NativeFeaNode(2, 0.0, 0.0, 3.0)));
 	nodes.insert(std::make_pair(3, new NativeFeaNode(3, 5.0, 0.0, 3.0)));
 
 	members = std::map<int, NativeFeaMember*>();
