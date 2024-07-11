5080126523a3cec109434c2c7c86e66b9dd0eae3
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-07-11 17:27:47 +0200

    TemplateMappingSetter

---
+++ src/ConnectionWebClient/ConnectionWebClient/Services/TemplateMappingSetter.cs
@@ -0,0 +1,23 @@
+﻿using IdeaStatiCa.Plugin.Api.ConnectionRest.Model.Model_Template;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ConnectionWebClient.Services
+{
+	public interface ITemplateMappingSetter
+	{
+		Task<TemplateConversions> SetAsync(TemplateConversions defaultConversions);
+	}
+
+	internal class TemplateMappingSetter : ITemplateMappingSetter
+	{
+		public async Task<TemplateConversions> SetAsync(TemplateConversions defaultConversions)
+		{
+			var defaultConversionsJson = JsonConvert.SerializeObject(defaultConversions);
+
+			await Task.CompletedTask;
+
+			var modifiedConversions = JsonConvert.DeserializeObject<TemplateConversions>(defaultConversionsJson);
+		}
+	}
+}
