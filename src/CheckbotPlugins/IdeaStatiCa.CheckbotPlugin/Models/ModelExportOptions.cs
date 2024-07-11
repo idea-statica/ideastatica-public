94c4b321f9ddd575c483f91d52f355925c254eb8
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2024-07-11 02:34:35 +0200

    Updated doc comments

--- src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/Models/ModelExportOptions.cs
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/Models/ModelExportOptions.cs
@@ -5,13 +5,11 @@
 		public static readonly ModelExportOptions Default = new ModelExportOptions();
 
 		/// <summary>
-		/// When <see langword="true"/> <see cref="IdeaRS.OpenModel.OpenModelContainer"/> will contains results.
+		/// When <see langword="true"/> OpenModelContainer will contains results.
 		/// </summary>
 		public bool WithResults { get; set; } = true;
 
-		/// <summary>
-		/// When <see langword="true"/> all cross-section in the IOM will be specified as <see cref="IdeaRS.OpenModel.CrossSection.CrossSectionComponent"/>.
-		/// </summary>
+		/// <remarks>Not implemented yet</remarks>
 		public bool AllCrossSectionsAsGeneral { get; set; } = false;
 	}
 }
