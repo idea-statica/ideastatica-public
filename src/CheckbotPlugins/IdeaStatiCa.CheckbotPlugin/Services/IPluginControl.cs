a061874582025a628b5118ec9d9b89cd23f7cf94
Dalibor Bačovský <dalibor.bacovsky@ideastatica.com>
2024-07-16 03:15:56 +0200

    Added plugin control interface

---
+++ src/CheckbotPlugins/IdeaStatiCa.CheckbotPlugin/Services/IPluginControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.CheckbotPlugin.Services
+{
+	public enum StopReason
+	{
+		Event,
+		Call
+	}
+
+	public interface IPluginControl
+	{
+		event Action<StopReason> StopInvoked;
+
+		IPluginControlBehaviour Behaviour { get; set; }
+
+		Task AnnounceReady();
+
+		void Stop();
+	}
+}
