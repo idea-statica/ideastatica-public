f5e34ce7e0498ce35f5e16383b4616d3b4630c4d
pavelvadura <pavel.vadura@ideastatica.com>
2024-09-19 08:39:50 +0200

    fix iom grids serilization

--- src/IdeaRS.OpenModel/Connection/PinGrid.cs
+++ src/IdeaRS.OpenModel/Connection/PinGrid.cs
@@ -1,18 +1,23 @@
-﻿namespace IdeaRS.OpenModel.Connection
+﻿using System.Runtime.Serialization;
+
+namespace IdeaRS.OpenModel.Connection
 {
 	/// <summary>
 	/// Data of the pin grid
 	/// </summary>
+	[DataContract]
 	public class PinGrid : FastenerGridBase
 	{
 		/// <summary>
 		/// Replaceable pin
 		/// </summary>
+		[DataMember]
 		public bool IsReplaceable { get; set; }
 
 		/// <summary>
 		/// Pin
 		/// </summary>
+		[DataMember]
 		public ReferenceElement Pin { get; set; }
 	}
 }
