f6f8eec9305debab14e77ccfe3aa230b0d42a39d
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-14 23:27:47 +0200

    UpdateProjectFromIomFileAsync

--- src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/ApiExt/ProjectApiExt.cs
+++ src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/ApiExt/ProjectApiExt.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,18 +23,22 @@ namespace IdeaStatiCa.ConnectionApi.Api
 		/// 
 		/// </summary>
 		/// <param name="filePath"></param>
+		/// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
 		/// <returns></returns>
-		Task<ConProject> OpenProjectAsync(string filePath);
+		Task<ConProject> OpenProjectAsync(string filePath, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
 
-		Task SaveProjectAsync(Guid projectId, string fileName);
+		Task SaveProjectAsync(Guid projectId, string fileName, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
 
 		/// <summary>
 		/// 
 		/// </summary>
 		/// <param name="iomFilePath"></param>
 		/// <param name="connectionsToCreate"></param>
+		/// <param name="cancellationToken">Cancellation Token to cancel the request.</param>
 		/// <returns></returns>
-		Task<ConProject> CreateProjectFromIomFileAsync(string iomFilePath, List<int> connectionsToCreate = null);
+		Task<ConProject> CreateProjectFromIomFileAsync(string iomFilePath, List<int> connectionsToCreate = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
+
+		Task<ConProject> UpdateProjectFromIomFileAsync(Guid projectId, string iomFilePath, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken));
 	}
 
 	/// <summary>
@@ -61,7 +64,7 @@ namespace IdeaStatiCa.ConnectionApi.Api
 		/// </summary>
 		public ConProject ActiveProjectData { get; private set; } = null;
 
-		public async Task<ConProject> OpenProjectAsync(string path)
+		public async Task<ConProject> OpenProjectAsync(string path, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
 		{
 			//await CreateAsync();
 
@@ -71,7 +74,7 @@ namespace IdeaStatiCa.ConnectionApi.Api
 				{
 					await fs.CopyToAsync(ms);
 					ms.Seek(0, System.IO.SeekOrigin.Begin);
-					var conProject = await OpenProjectAsync(ms);
+					var conProject = await OpenProjectAsync(ms, 0, cancellationToken);
 					this.ActiveProjectData = conProject;
 				}
 			}
@@ -79,9 +82,9 @@ namespace IdeaStatiCa.ConnectionApi.Api
 			return this.ActiveProjectData;
 		}
 
-		public async Task SaveProjectAsync(Guid projectId, string fileName)
+		public async Task SaveProjectAsync(Guid projectId, string fileName, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
 		{
-			var response = await base.DownloadProjectWithHttpInfoAsync(projectId, "application/octet-stream");
+			var response = await base.DownloadProjectWithHttpInfoAsync(projectId, "application/octet-stream", 0, cancellationToken);
 			byte[] buffer = (byte[])response.Data;
 			using (var fileStream = System.IO.File.Create(fileName))
 			{
@@ -89,7 +92,7 @@ namespace IdeaStatiCa.ConnectionApi.Api
 			}
 		}
 
-		public async Task<ConProject> CreateProjectFromIomFileAsync(string fileName, List<int> connectionsToCreate = null)
+		public async Task<ConProject> CreateProjectFromIomFileAsync(string fileName, List<int> connectionsToCreate = null, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
 		{
 			string xmlString = string.Empty;
 #if NETSTANDARD2_1_OR_GREATER
@@ -104,16 +107,37 @@ namespace IdeaStatiCa.ConnectionApi.Api
 			{
 				memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
 
-				var response = await ImportIOMWithHttpInfoAsync(memoryStream, connectionsToCreate);
+				var response = await ImportIOMWithHttpInfoAsync(memoryStream, connectionsToCreate, null, 0, cancellationToken);
 
 				return response.Data;
+			}
+		}
+
+		public async Task<ConProject> UpdateProjectFromIomFileAsync(Guid projectId, string iomFilePath, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
+		{
+			string xmlString = string.Empty;
+#if NETSTANDARD2_1_OR_GREATER
+			xmlString = await System.IO.File.ReadAllTextAsync(iomFilePath);
+#else
+			xmlString = System.IO.File.ReadAllText(iomFilePath);
+
+#endif
+			xmlString = xmlString.Replace("utf-16", "utf-8");
+
+			using (var iomStream = new System.IO.MemoryStream(Encoding.UTF8.GetBytes(xmlString)))
+			{
+				iomStream.Seek(0, System.IO.SeekOrigin.Begin);
+
+				var response = await UpdateFromIOMAsync(projectId, iomStream, 0, cancellationToken);
+
+				return response;
 			}
 		}
 
 		/// <inheritdoc cref="ProjectApi.GetSetupAsync(Guid, int, System.Threading.CancellationToken)"/>
 		public new async System.Threading.Tasks.Task<ConnectionSetup> GetSetupAsync(Guid projectId, int operationIndex = 0, System.Threading.CancellationToken cancellationToken = default(System.Threading.CancellationToken))
 		{
-				var response = await GetSetupWithHttpInfoAsync(projectId);
+				var response = await GetSetupWithHttpInfoAsync(projectId, null, 0, cancellationToken);
 				var setup = JsonConvert.DeserializeObject<ConnectionSetup>(response.RawContent, IdeaJsonSerializerSetting.GetJsonSettingIdea());
 				return setup;
 		}
@@ -141,7 +165,7 @@ namespace IdeaStatiCa.ConnectionApi.Api
 			localVarRequestOptions.Data = JsonConvert.SerializeObject(connectionSetup, IdeaJsonSerializerSetting.GetJsonSettingIdea());
 
 			// make the HTTP request
-			var localVarResponse = await this.AsynchronousClient.PutAsync<ConnectionSetup>("/api/1/projects/{projectId}/connection-setup", localVarRequestOptions, this.Configuration, cancellationToken).ConfigureAwait(false);
+			var localVarResponse = await this.AsynchronousClient.PutAsync<ConnectionSetup>("/api/1/projects/{projectId}/connection-setup", localVarRequestOptions, this.Configuration, cancellationToken);
 
 			if (this.ExceptionFactory != null)
 			{
