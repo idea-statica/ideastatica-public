f3ed5337a947c56a9e38e758148f355b7428843d
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-22 10:29:12 +0200

    default path to IdeaStativa v24.1 installation dir

--- examples/api/csharp/connection/Connection-API-WinForm-Tester/ConnectionAutomationApp/Properties/Settings.Designer.cs
+++ examples/api/csharp/connection/Connection-API-WinForm-Tester/ConnectionAutomationApp/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace ConnectionAutomationApp.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 24.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files\\IDEA StatiCa\\StatiCa 24.1")]
         public string IdeaStatiCaDir {
             get {
                 return ((string)(this["IdeaStatiCaDir"]));
