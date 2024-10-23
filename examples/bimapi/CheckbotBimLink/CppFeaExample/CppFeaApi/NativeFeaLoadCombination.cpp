f05e20c2815e62623cb5189ceba6593626e565ce
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-23 10:29:46 +0200

    NativeFeaLoadCombination

--- examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApi/NativeFeaLoadCombination.cpp
+++ examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApi/NativeFeaLoadCombination.cpp
@@ -1,2 +1,20 @@
 #include "pch.h"
 #include "NativeFeaLoadCombination.h"
+
+NativeFeaLoadCombination::NativeFeaLoadCombination()
+{
+	this->CombiFactors = std::vector<NativeFeaCombiFactor>();
+	this->Name = L"";
+	this->Type = 0;
+	this->Id = 0;
+	this->Category = 0;
+}
+
+NativeFeaLoadCombination::NativeFeaLoadCombination(int id, std::wstring name, int category, int type, std::vector<NativeFeaCombiFactor>& combiFactors)
+{
+	this->Id = id;
+	this->Name = name;
+	this->Category = category;
+	this->Type = type;
+	this->CombiFactors = combiFactors;
+}
