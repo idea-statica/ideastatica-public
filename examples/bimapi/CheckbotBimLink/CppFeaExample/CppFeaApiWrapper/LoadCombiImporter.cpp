d121fe034f6b84d45c6adad26a0283e4625d6d89
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-10-24 16:44:36 +0200

    LoadCombiImporter

--- examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApiWrapper/LoadCombiImporter.cpp
+++ examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApiWrapper/LoadCombiImporter.cpp
@@ -13,7 +13,56 @@ namespace CppFeaApiWrapper
 
 		IIdeaCombiInput^ LoadCombiImporter::Create(int id)
 		{
-			throw gcnew System::NotImplementedException();
+			auto feaLoadCombi = this->context->GetLoading()->GetLoadCombination(id);
+
+			auto ideaCombi = gcnew IdeaCombiInput(id);
+			ideaCombi->Name = gcnew String(feaLoadCombi->Name.c_str());
+			ideaCombi->TypeCombiEC = GetCombiAction(feaLoadCombi->Category);
+			ideaCombi->TypeCalculationCombi = GetCalculationType(feaLoadCombi->Type);
+			ideaCombi->CombiItems = GetCombiInputs(feaLoadCombi->CombiFactors, id);
+
+			return ideaCombi;
+		}
+
+		List<IIdeaCombiItem^>^ LoadCombiImporter::GetCombiInputs(std::vector<NativeFeaCombiFactor>* loadFactors, int combiId)
+		{
+			List<IIdeaCombiItem^>^ combiItems = gcnew List<IIdeaCombiItem^>();
+
+			for (auto item : *loadFactors)
+			{
+				String^ itemName = String::Format("Combi {0}_{1}", combiId, item.LoadCaseId);
+				IdeaCombiItem^ combiItem = gcnew IdeaCombiItem(itemName);
+				combiItem->Coeff = item.CombiMultiplier;
+				combiItem->LoadCase = GetMaybe<IIdeaLoadCase^>(item.LoadCaseId);
+				combiItems->Add(combiItem);
+			}
+
+			return combiItems;
+		}
+
+		IdeaRS::OpenModel::Loading::TypeCalculationCombiEC LoadCombiImporter::GetCalculationType(int loadCombiType)
+		{
+			switch (loadCombiType)
+			{
+				case NativeFeaLoads::Combi_Type_Linear:
+					return TypeCalculationCombiEC::Linear;
+
+				default:
+					return TypeCalculationCombiEC::Linear;
+			}
+		}
+
+		IdeaRS::OpenModel::Loading::TypeOfCombiEC LoadCombiImporter::GetCombiAction(int combiCategory)
+		{
+			switch (combiCategory)
+			{
+			case NativeFeaLoads::Combi_Category_ULS:
+				return TypeOfCombiEC::ULS;
+			case NativeFeaLoads::Combi_Category_SLS:
+				return TypeOfCombiEC::SLS_Char;
+			default:
+				return TypeOfCombiEC::ULS;
+			}
 		}
 	}
 }
--- examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApiWrapper/LoadCombiImporter.h
+++ examples/bimapi/CheckbotBimLink/CppFeaExample/CppFeaApiWrapper/LoadCombiImporter.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "ImporterContext.h"
+#include <iostream>
+#include <vector>
 
 using namespace IdeaStatiCa::BimApi;
 using namespace IdeaStatiCa::BimApiLink;
@@ -9,6 +11,7 @@ using namespace IdeaStatiCa::BimApiLink::Identifiers;
 using namespace IdeaRS::OpenModel::Loading;
 using namespace ImporterWrappers;
 using namespace System;
+using namespace System::Collections::Generic;
 
 namespace CppFeaApiWrapper
 {
@@ -18,12 +21,16 @@ namespace CppFeaApiWrapper
 		{
 		private:
 			ImporterContext^ context;
-
+			List<IIdeaCombiItem^>^ GetCombiInputs(std::vector<NativeFeaCombiFactor>* loadFactors, int combiId);
 
 		public:
 			LoadCombiImporter(ImporterContext^ context);
 
 			virtual IIdeaCombiInput^ Create(int id) override;
+
+			static IdeaRS::OpenModel::Loading::TypeCalculationCombiEC GetCalculationType(int loadCombiType);
+
+			static IdeaRS::OpenModel::Loading::TypeOfCombiEC GetCombiAction(int combiCategory);
 		};
 	}
 }
