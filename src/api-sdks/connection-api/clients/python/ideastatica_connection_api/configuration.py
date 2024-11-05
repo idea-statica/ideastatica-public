5423495ab4d278432aefac13ff4b469c1ca3b69a
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-11-05 12:35:01 +0100

    IS v24.1.1.1116 - modified examples

--- src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
+++ src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
@@ -384,7 +384,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0\n"\
-               "SDK Package Version: 24.1.1.1036".\
+               "SDK Package Version: 24.1.1.1116".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- src/api-sdks/connection-api/clients/python/setup.py
+++ src/api-sdks/connection-api/clients/python/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "ideastatica-connection-api"
-VERSION = "24.1.1.1036"
+VERSION = "24.1.1.1116"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
