3f1bfec3158df0122a58504278b555a64890b56e
pavelvadura <pavel.vadura@ideastatica.com>
2024-11-06 13:24:10 +0100

    23796 iom boltgrade fix atribute

--- src/IdeaRS.OpenModel/Libraries/Material/MaterialBoltGradeEc2.cs
+++ src/IdeaRS.OpenModel/Libraries/Material/MaterialBoltGradeEc2.cs
@@ -6,7 +6,7 @@ namespace IdeaRS.OpenModel.Material
 	/// <summary>
 	/// Material bolt grade
 	/// </summary>
-	[OpenModelClass("CI.StructModel.Libraries.Material.MaterialBoltGrade CI.Material", "CI.StructModel.Libraries.Material.IMatBoltGrade, CI.BasicTypes", typeof(MaterialBoltGrade))]
+	[OpenModelClass("CI.StructModel.Libraries.Material.MaterialBoltGrade, CI.Material", "CI.StructModel.Libraries.Material.IMatBoltGrade, CI.BasicTypes", typeof(MaterialBoltGrade))]
 	[DataContract]
 	[JsonObject(MemberSerialization = MemberSerialization.OptOut)]
 	public class MaterialBoltGradeEc2 : MaterialBoltGrade
