a2ba9b56bce84ee5ce8d8a77eff76cc64f5f2eba
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-11-09 19:56:08 +0100

    opening / closing project

---
+++ src/api-sdks/connection-api/clients/csharp/examples/ConApiWpfClient/ConApiWpfClientApp/ViewModels/JsonEditorViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ConApiWpfClientApp.ViewModels
+{
+	public class JsonEditorViewModel : ViewModelBase
+	{
+		private string? editedText;
+
+		public string? EditedText
+		{
+			get => editedText;
+			set
+			{
+				SetProperty(ref editedText, value);
+			}
+		}
+	}
+}
---
+++ src/api-sdks/connection-api/clients/csharp/examples/ConApiWpfClient/ConApiWpfClientApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ConApiWpfClientApp.ViewModels
+{
+	public class ViewModelBase : ObservableObject
+	{
+	}
+}
