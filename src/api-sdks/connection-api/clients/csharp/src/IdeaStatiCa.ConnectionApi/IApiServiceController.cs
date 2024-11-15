26c62687a2aec9a9db4604936a51766eb69dd0e9
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2024-11-15 23:07:00 +0100

    Create ServiceRunner for Conn api to provide funcionality of automated service startin. Rename IConnectionClientFactory to IApiServiceController

---
+++ src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/IApiServiceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.ConnectionApi
+{
+	/// <summary>
+	/// Factory for creating instances of clients/>
+	/// </summary>
+	public interface IApiServiceController<TClient>
+	{
+		/// <summary>
+		/// Create an instance of IConnectionApiClient
+		/// </summary>
+		/// <returns>Instance of client</returns>
+		Task<TClient> CreateConnectionApiClient();
+	}
+}
