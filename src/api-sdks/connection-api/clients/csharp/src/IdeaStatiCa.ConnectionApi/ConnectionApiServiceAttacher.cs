e91711fd46f129cbb66a720df44e5f5eff50dbf8
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2024-11-25 23:59:35 +0100

    remove Task.FromResult

--- src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/ConnectionApiServiceAttacher.cs
+++ src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/ConnectionApiServiceAttacher.cs
@@ -20,7 +20,7 @@ namespace IdeaStatiCa.ConnectionApi
 		{
 			var client = new ConnectionApiClient(BaseUrl);
 			await client.CreateAsync();
-			return await Task.FromResult(client);
+			return client;
 		}
 	}		
 }
