f93e5b2582594bd1e1f7231263dc2326fa6393d7
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2024-11-28 11:22:41 +0100

    Replace ObsoleteRcs with new Rcs. Remove DEBUG statement. set Timeout to 120 seconds

--- src/IdeaStatiCa.RcsClient/Factory/RcsClientFactory.cs
+++ src/IdeaStatiCa.RcsClient/Factory/RcsClientFactory.cs
@@ -93,16 +93,13 @@ namespace IdeaStatiCa.RcsClient.Factory
 						rcsRestApiProcess = new Process();
 						rcsRestApiProcess.StartInfo.FileName = apiExecutablePath;
 						rcsRestApiProcess.StartInfo.Arguments = arguments;
-						rcsRestApiProcess.StartInfo.UseShellExecute = false;
-#if !DEBUG
-						rcsRestApiProcess.StartInfo.CreateNoWindow = true;
-#endif
+						rcsRestApiProcess.StartInfo.UseShellExecute = true;
 						rcsRestApiProcess.Start();
 
 						// Check if the API process is ready
 						var apiUrlBase = new Uri($"{LOCALHOST_URL}:{port}");
 						var apiUrlHeartbeat = new Uri(apiUrlBase, RestApiConstants.RestApiHeartbeat);
-						var cts = new CancellationTokenSource(TimeSpan.FromSeconds(300));
+						var cts = new CancellationTokenSource(TimeSpan.FromSeconds(120));
 						var isApiReady = await WaitForApiToBeReady(apiUrlHeartbeat, cts.Token);
 
 						if (isApiReady && !rcsRestApiProcess.HasExited)
