957da7aa88a6270f3c624cd87c3859724aea98ce
Martin Pospisil <martin.pospisil@ideastatica.com>
2024-12-03 11:50:55 +0100

    IS v24.1.2.1248 - con rest api

--- src/api-sdks/connection-api/clients/python/setup.py
+++ src/api-sdks/connection-api/clients/python/setup.py
@@ -22,14 +22,13 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "ideastatica-connection-api"
-VERSION = "24.1.1.1418"
+VERSION = "24.1.2.1248"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
     "python-dateutil >= 2.8.2",
     "pydantic >= 2",
     "typing-extensions >= 4.7.1",
-    "aiohttp >= 3.11.7",
 ]
 
 setup(
