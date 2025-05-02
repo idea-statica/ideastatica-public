75da1691b47b424f03c9203695e8d1b6fa083f7d
Tomas Kohoutek <tomas.kohoutek@ideastatica.com>
2025-05-02 12:39:11 +0200

    Parameter update object

--- src/IdeaStatiCa.Api/Connection/Model/Parameter/IdeaParameter.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Parameter/IdeaParameter.cs
@@ -1,4 +1,6 @@
-﻿namespace IdeaStatiCa.Api.Connection.Model
+﻿using System.Collections.Generic;
+
+namespace IdeaStatiCa.Api.Connection.Model
 {
 	public class IdeaParameterUpdate
 	{
@@ -19,16 +21,54 @@
 
 		public string ParameterType { get; set; }
 
-		public string ValidationExpression { get; set; }
-
 		public string Description { get; set; }
-
-		public string ValidationStatus { get; set; }
 
 		public bool? IsVisible { get; set; }
 
+		public IdeaParameterValidation ParameterValidation { get; set; }
+	}
+
+	public class IdeaParameterValidation
+	{
+		public string ValidationStatus { get; set; }
+
+		public string ValidationExpression { get; set; }
+
+		public bool ValidationExpressionEvaluated { get; set; }
+
 		public string LowerBound { get; set; }
 
+		public double LowerBoundEvaluated { get; set; }
+
 		public string UpperBound { get; set; }
+
+		public double UpperBoundEvaluated { get; set; }
+
+		public string Message { get; set; }
+	}
+
+	public class IdeaParameterValidationResponse
+	{
+		public string Key { get; set; }
+
+		public string Message { get; set; }
+
+		public string ValidationStatus { get; set; }
+
+		public IdeaParameterValidationResponse(IdeaParameter failedParameter)
+		{
+			Key = failedParameter.Key;
+			Message = failedParameter.ParameterValidation.Message;
+			ValidationStatus = failedParameter.ParameterValidation.ValidationStatus;
+		}
+	}
+
+	public class ParameterUpdateResponse
+	{
+		public bool SetToModel { get; set; } = true;
+
+		public List<IdeaParameterValidationResponse> FailedValidations { get; set; } = new List<IdeaParameterValidationResponse>();
+
+		public List<IdeaParameter> Parameters { get; set; } = new List<IdeaParameter>();
 	}
 }
