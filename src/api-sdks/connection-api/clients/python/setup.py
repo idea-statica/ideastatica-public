556fdf04079af627774d1893080e463b889dfd2f
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-05-12 15:28:24 +0200

    setup.py VERSION = "25.0.1.960"

--- src/api-sdks/connection-api/clients/python/setup.py
+++ src/api-sdks/connection-api/clients/python/setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "ideastatica-connection-api"
-VERSION = "25.0.0.1972"
+VERSION = "25.0.1.960"
 PYTHON_REQUIRES = ">= 3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 3.0.0",
