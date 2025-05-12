f949e385affa26c619343e94a0506fd3a64468ad
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-05-12 14:48:39 +0200

    python client

--- src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
+++ src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "24.1.3.2377"
+__version__ = "25.0.1.0960"
 
 # import apis into sdk package
 from ideastatica_rcs_api.api.calculation_api import CalculationApi
--- src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/configuration.py
+++ src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/configuration.py
@@ -385,7 +385,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.0\n"\
-               "SDK Package Version: 24.1.3.2377".\
+               "SDK Package Version: 25.0.1.0960".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
