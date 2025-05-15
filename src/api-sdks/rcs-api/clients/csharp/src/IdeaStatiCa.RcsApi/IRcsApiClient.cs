76f93cf4047043b3a41c57a65de9cba62a375bd7
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-05-15 13:59:11 +0200

    Add common interface for APIs

--- src/api-sdks/rcs-api/clients/csharp/src/IdeaStatiCa.RcsApi/IRcsApiClient.cs
+++ src/api-sdks/rcs-api/clients/csharp/src/IdeaStatiCa.RcsApi/IRcsApiClient.cs
@@ -1,22 +1,17 @@
-﻿using IdeaStatiCa.RcsApi.Api;
+﻿using IdeaStatiCa.Api.Common;
+using IdeaStatiCa.RcsApi.Api;
 using System;
-using System.Threading.Tasks;
 
 namespace IdeaStatiCa.RcsApi
 {
 	/// <summary>
 	/// Client for accessing IdeaStatiCa.ConnectionRestApi
 	/// </summary>
-	public interface IRcsApiClient : IDisposable
+	public interface IRcsApiClient : IApiClient
 #if NETSTANDARD2_1_OR_GREATER
 		, IAsyncDisposable
 #endif
 	{
-		///// <summary>
-		///// ClientID - assigned by the service
-		///// </summary>
-		//string ClientId { get; }
-
 		/// <summary>
 		/// Get Calculation API
 		/// </summary>
@@ -46,11 +41,5 @@ namespace IdeaStatiCa.RcsApi
 		/// Get Section API
 		/// </summary>
 		ISectionApiAsync Section { get; }
-
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <returns></returns>
-		Task CreateAsync();
 	}
 }
