8d9be9b10eea62728b5bf9016061fdce54033049
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-05-21 15:27:42 +0200

    Update puthon client

--- src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/Client/Configuration.cs
+++ src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/Client/Configuration.cs
@@ -34,7 +34,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "25.0.1.0960";
+        public const string Version = "25.0.2.0905";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -113,7 +113,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/25.0.1.0960/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/25.0.2.0905/csharp");
             BasePath = "http://localhost";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -536,7 +536,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0\n";
-            report += "    SDK Package Version: 25.0.1.0960\n";
+            report += "    SDK Package Version: 25.0.2.0905\n";
 
             return report;
         }
