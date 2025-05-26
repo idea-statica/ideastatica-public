8c60feb6c2b4cecf7cec44dfaf87aa85d6970c69
OndraZitko <139956356+OndraZitko@users.noreply.github.com>
2025-05-26 22:52:28 +0200

    Add common interface for APIs (#879)

---
+++ src/IdeaStatiCa.Api/Common/IApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.Api.Common
+{
+	public interface IApiClient : IDisposable
+	{
+		/// <summary>
+		/// Innialize instance of client
+		/// </summary>
+		/// <returns></returns>
+		Task CreateAsync();
+	}
+}
