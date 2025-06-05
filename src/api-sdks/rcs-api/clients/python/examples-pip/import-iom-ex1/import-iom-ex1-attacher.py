8e4792799727a66b273dd40d4551fcbbe5acc1ac
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-06-05 11:25:58 +0200

    import iom example

--- src/api-sdks/rcs-api/clients/python/examples-pip/import-iom-ex1/import-iom-ex1-attacher.py
+++ src/api-sdks/rcs-api/clients/python/examples-pip/import-iom-ex1/import-iom-ex1-attacher.py
@@ -1,7 +1,21 @@
+import sys
 import os
 import asyncio
 from pprint import pprint
 from urllib.parse import urljoin
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+iom_file_path = os.path.join(dir_path, r'..\..\projects', 'ImportOpenModel.xml')
+rcs_project_file_path = os.path.join(dir_path, r'..\..\projects', 'Project1.ideaRcs')
+
+# uncomment these lines to use the local ideastatica_rcs_api
+
+# # Get the parent directory
+# parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), r'../..'))
+
+# # Add the parent directory to sys.path
+# sys.path.append(parent_dir)
+
 import ideastatica_rcs_api
 import ideastatica_rcs_api.rcs_api_service_attacher as rcs_api_service_attacher
 
@@ -13,23 +27,25 @@ configuration = ideastatica_rcs_api.Configuration(
     host = baseUrl
 )
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-iom_file_path = os.path.join(dir_path, r'..\..\projects', 'ImportOpenModel.xml')
-rcs_project_file_path = os.path.join(dir_path, r'..\..\projects', 'Project1.ideaRcs')
 
 async def main():
     with rcs_api_service_attacher.RcsApiServiceAttacher(baseUrl).create_api_client() as api_client:
-        # Open project
-        # project_data = api_client.project.open_project_from_file(rcs_project_file_path)
-        
         # Import project from IOM file
-        project_data = api_client.project.import_iom_file(iom_file_path)
+        project_data = api_client.project.create_project_from_iom_file(iom_file_path)
 
-        # Get the project data
-        project_data2 = api_client.project.get_active_project()
+        calcParams = ideastatica_rcs_api.RcsCalculationParameters()
+        calcParams.sections = [project_data.sections[0].id]
 
-        # api_client.project.save_project(project_data.project_id, r'c:\x\pro1.idearcs')
+            # run stress-strain analysis for the connection
+        cal_results = api_client.calculation.calculate(api_client.project.active_project_id, calcParams)
 
-        api_client.project.close_project(project_data2.project_id)
+        section_results = cal_results[0]
+
+        print(f"Results section id : {section_results.section_id}\n")
+        for item in section_results.overall_items:
+            print(f"Status: {item.result_type} Check Value: {item.check_value}")
+
+        api_client.project.save_project(project_data.project_id, r'c:\x\proj1.ideaRcs')
+
 
 asyncio.run(main())
