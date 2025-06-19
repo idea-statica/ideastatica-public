57ba5e6c2caea0026eb48614ad3d87326d2288b3
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-06-19 16:03:40 +0200

    #27512 adding Member2DReactions

---
+++ src/IdeaRS.OpenModel/Result/Member2DReactions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace IdeaRS.OpenModel.Result
+{
+	[Obfuscation(Feature = "renaming")]
+	public class Member2DReactions
+	{
+		public Member2DReactions()
+		{
+			BoundaryReactions = new List<ResultOnMember>();
+		}
+
+		/// <summary>
+		/// Loading
+		/// </summary>
+		public IdeaRS.OpenModel.Result.Loading Loading { get; set; }
+
+		public int MemberId { get; set; }
+
+		public List<ResultOnMember> BoundaryReactions { get; set; }
+	}
+}
