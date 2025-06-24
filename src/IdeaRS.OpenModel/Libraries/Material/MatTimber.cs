41d351cb54d543fcb6f9c8eabf76710821701c7c
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-06-24 08:50:09 +0200

    serilaization fix

--- src/IdeaRS.OpenModel/Libraries/Material/MatTimber.cs
+++ src/IdeaRS.OpenModel/Libraries/Material/MatTimber.cs
@@ -1,8 +1,10 @@
 ﻿using Newtonsoft.Json;
 using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace IdeaRS.OpenModel.Material
 {
+	[XmlInclude(typeof(MatTimberEc5)]
 	[DataContract]
 	[JsonObject(MemberSerialization = MemberSerialization.OptOut)]
 	public abstract class MatTimber : Material
