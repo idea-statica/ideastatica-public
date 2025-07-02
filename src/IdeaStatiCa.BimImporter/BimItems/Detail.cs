a8b9e36fe2700d0eeb95c16d74a89d46ad727710
Robin Blazek <robin.blazek@ideastatica.com>
2025-07-02 14:09:33 +0200

    #28052 New BIMItem for detail

---
+++ src/IdeaStatiCa.BimImporter/BimItems/Detail.cs
@@ -0,0 +1,27 @@
+﻿using IdeaStatiCa.BimApi;
+using IdeaStatiCa.Plugin;
+
+namespace IdeaStatiCa.BimImporter.BimItems
+{
+	public class Detail : IBimItem
+	{
+		/// <summary>
+		/// Gets the type of the bim item.
+		/// </summary>
+		public BIMItemType Type => BIMItemType.Detail;
+
+		/// <summary>
+		/// Gets the referenced bim object.
+		/// </summary>
+		public IIdeaObject ReferencedObject { get; }
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Detail"/> class.
+		/// </summary>
+		/// <param name="member2D">The member2D.</param>
+		public Detail(IIdeaMember2D member2D)
+		{
+			ReferencedObject = member2D;
+		}
+	}
+}
