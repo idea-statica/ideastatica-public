47464772c3964fda6f04963428bf9a9a81e5f7dc
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-07-15 14:37:54 +0200

    adding ResultsForSelection

---
+++ src/IdeaRS.OpenModel/Result/ResultsForSelection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace IdeaRS.OpenModel.Result
+{
+	/// <summary>
+	/// Results for group of structural elements
+	/// </summary>
+	/// <typeparam name="T">Type of result</typeparam>
+	public class ResultsForSelection<T>
+	{
+		public List<T> Results { get; set; }
+	}
+}
