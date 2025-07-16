f79837627c2bbe397392ded1e509f49c6743241a
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-07-16 21:49:43 +0200

    Create ValuesInPoint and add them to Member2DReactions

--- src/IdeaRS.OpenModel/Result/Member2DReactExtensions.cs
+++ src/IdeaRS.OpenModel/Result/Member2DReactExtensions.cs
@@ -50,10 +50,56 @@ namespace IdeaRS.OpenModel.Result
 		/// <param name="loadingInx">Index of the requested loading in <see cref="IdeaRS.OpenModel.Result.Member2DReactions{T}.Loadings"/></param>
 		/// <param name="valInx">the value index (from 0 to <see cref="IdeaRS.OpenModel.Result.ValuesInSegmentSections{T}.ValueCountInSection"/>) - 1)</param>
 		/// <returns></returns>
-		public static T GetValue<T>(this ValuesInSegmentSections<T> src, int sectionInx, int loadingInx, int valInx) where T : struct
+		public static T GetValueInSection<T>(this ValuesInSegmentSections<T> src, int sectionInx, int loadingInx, int valInx) where T : struct
 		{
 			int sectionIndex = GetSectionValueIndex<T>(src, sectionInx, loadingInx);
 			return src.Values[sectionIndex + valInx];
+		}
+
+		/// <summary>
+		/// Calculates the index of a point values based on the specified loading index.
+		/// </summary>
+		/// <typeparam name="T">The type of the values in the point, which must be a value type.</typeparam>
+		/// <param name="src">The source <see cref="ValuesInPoint{T}"/> instance containing the values.</param>
+		/// <param name="loadingIdx">The loading index used to calculate the point value index. Must be a non-negative integer.</param>
+		/// <returns>The calculated index of the point value within the section.</returns>
+		public static int GetPointValueIndex<T>(this ValuesInPoint<T> src, int loadingIdx) where T : struct
+		{
+			return src.ValueCountInSection * loadingIdx;
+		}
+
+		/// <summary>
+		/// Sets the values in a specified section of the <see cref="ValuesInPoint{T}"/> object.
+		/// </summary>
+		/// <remarks>This method updates the values in the specified section of the <paramref name="src"/> object,
+		/// starting at the position determined by <paramref name="loadingIdx"/>. Ensure that the <paramref name="values"/>
+		/// list contains enough elements to fill the section, as defined by the <see
+		/// cref="ValuesInPoint{T}.ValueCountInSection"/> property.</remarks>
+		/// <typeparam name="T">The type of the values, which must be a value type.</typeparam>
+		/// <param name="src">The <see cref="ValuesInPoint{T}"/> instance where the values will be set.</param>
+		/// <param name="loadingIdx">The index of the loading case that determines the starting position for setting values.</param>
+		/// <param name="values">A list of values to set in the specified point.</param>
+		public static void SetValuesInPoint<T>(this ValuesInPoint<T> src, int loadingIdx, IList<T> values) where T : struct
+		{
+			int loadCaseIndex = GetPointValueIndex(src, loadingIdx);
+			for(int i = 0; i < src.ValueCountInSection; i++)
+			{
+				src.Values[loadCaseIndex + i] = values[i];
+			}
+		}
+
+		/// <summary>
+		/// Retrieves a value from the specified point in a collection of values.
+		/// </summary>
+		/// <typeparam name="T">The type of the value to retrieve, which must be a value type.</typeparam>
+		/// <param name="src">The source collection of values from which to retrieve the value.</param>
+		/// <param name="loadingIdx">The index representing the loading case in the collection.</param>
+		/// <param name="valueIdx">The index of the specific value within the loading case to retrieve.</param>
+		/// <returns>The value of type <typeparamref name="T"/> located at the specified indices.</returns>
+		public static T GetValueInPoint<T>(this ValuesInPoint<T> src, int loadingIdx, int valueIdx) where T : struct
+		{
+			int loadCaseIndex = GetPointValueIndex(src, loadingIdx);
+			return src.Values[loadCaseIndex + valueIdx];
 		}
 	}
 }
