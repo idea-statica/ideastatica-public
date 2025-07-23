4a9d03e931b50b385215efc979fd64db5f366c3f
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-07-23 23:29:17 +0200

    python v.25.0.4.0896

--- src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
+++ src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
@@ -385,7 +385,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0\n"\
-               "SDK Package Version: 25.0.4.0875".\
+               "SDK Package Version: 25.0.4.0896".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
