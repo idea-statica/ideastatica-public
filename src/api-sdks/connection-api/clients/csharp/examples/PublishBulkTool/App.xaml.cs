64489fae8a35c3f6c4cc04038bb799782601c9c9
Tomas Kohoutek <tomas.kohoutek@ideastatica.com>
2025-08-07 07:10:02 +0200

    Publish bulk tool

---
+++ src/api-sdks/connection-api/clients/csharp/examples/PublishBulkTool/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Application = System.Windows.Application;
+
+namespace PublishBulkTool
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
