20e214e5520209db8283bbadcf555b8ab66125c2
Robin Blazek <robin.blazek@ideastatica.com>
2025-08-07 12:19:57 +0200

    #28816 New extended IFeaModel

---
+++ src/IdeaStatiCa.BimApiLink/Plugin/IFeaExtendedModel.cs
@@ -0,0 +1,18 @@
+﻿using IdeaStatiCa.BimApi;
+using IdeaStatiCa.BimApiLink.Identifiers;
+using System.Collections.Generic;
+
+namespace IdeaStatiCa.BimApiLink.Plugin
+{
+	/// <summary>
+	/// Extends standard IFeaModel by possibility to import also load cases not included in combinations
+	/// </summary>
+	public interface IFeaExtendedModel : IFeaModel
+	{
+		/// <summary>
+		/// Gets all load cases in current model in Fea application
+		/// </summary>
+		/// <returns>Identifiers of load cases</returns>
+		IEnumerable<Identifier<IIdeaLoadCase>> GetAllLoadCases();
+	}
+}
