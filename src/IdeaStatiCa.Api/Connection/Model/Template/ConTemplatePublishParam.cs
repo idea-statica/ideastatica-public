1b07a3d74fb5821ff72f3bf3b675606ab31bdbe1
Tomas Kohoutek <tomas.kohoutek@ideastatica.com>
2025-08-08 12:30:12 +0200

    Temporary remove templateAPI generated clients. Will be added for 25.1

--- src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplatePublishParam.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplatePublishParam.cs
@@ -5,5 +5,12 @@
 		public string Name { get; set; }
 		public string Author { get; set; }
 		public string CompanyName { get; set; }
+		public ConDesignSetType DesignSetType { get; set; } = ConDesignSetType.Company;
+	}
+
+	public enum ConDesignSetType
+	{
+		Private,
+		Company
 	}
 }
