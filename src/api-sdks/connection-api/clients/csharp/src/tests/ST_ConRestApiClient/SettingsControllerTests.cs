c413e57cbb2a0ea994e9b51d89bd672e0ad633bf
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-09-30 15:20:16 +0200

    adding ShouldGetProjectSettings

---
+++ src/api-sdks/connection-api/clients/csharp/src/tests/ST_ConRestApiClient/SettingsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using ST_ConnectionRestApi;
+
+namespace ST_ConRestApiClient
+{
+	public class SettingsControllerTests : ConRestApiBaseTest
+	{
+		[SetUp]
+		public async Task SetUp()
+		{
+
+			ConnectionApiClient = await ApiFactory.CreateApiClient();
+
+			string connProjectFilePath = Path.Combine(ProjectPath, "Simple-1-ECEN.ideaCon");
+			this.Project = await ConnectionApiClient.Project.OpenProjectAsync(connProjectFilePath);
+			this.ActiveProjectId = Project.ProjectId;
+			if (this.ActiveProjectId == Guid.Empty)
+			{
+				throw new Exception("Project is not opened");
+			}
+
+			this.Project = await ConnectionApiClient.Project.GetProjectDataAsync(ActiveProjectId);
+
+			Project.Should().NotBeNull();
+			Project.ProjectId.Should().NotBe(Guid.Empty);
+		}
+
+		[TearDown]
+		public async Task TearDown()
+		{
+			if (ConnectionApiClient != null)
+			{
+				await ConnectionApiClient!.DisposeAsync();
+			}
+		}
+
+		[Test]
+		public async Task ShouldGetProjectSettings()
+		{
+			var projectSettings = await ConnectionApiClient!.Settings.GetSettingsAsync(ActiveProjectId);
+			projectSettings.Count.Should().Be(167);
+		}
+	}
+}
