d3e4b5b31457e292f3a727ebbb7b63b48bbdabac
Martin Pospisil <mpospisil@users.noreply.github.com>
2025-10-04 09:52:12 +0200

    Rcsapi apply template example (#996)
    * example download
    
    * ReinforcedCrossSectionViewModel
    
    * updating section
    
    * RcsApiWpfClientApp example - apply templates
    
    * added create-rf-section1-attacher.py
    
    ---------
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

---
+++ src/api-sdks/rcs-api/clients/csharp/examples/RcsApiWpfClient/RcsApiWpfClientApp/Services/IReinforcedCrossSectionTemplateProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace IdeaStatiCa.RcsClient.Services
+{
+	public interface IReinforcedCrossSectionTemplateProvider
+	{
+		Task<string> GetTemplateAsync();
+	}
+}
