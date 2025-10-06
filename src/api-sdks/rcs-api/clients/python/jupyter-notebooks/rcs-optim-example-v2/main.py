7f701771df260665864ce2d0650c3e7578b29248
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-10-06 13:59:10 +0200

    calc_rcs_crack_width

--- src/api-sdks/rcs-api/clients/python/jupyter-notebooks/rcs-optim-example-v2/main.py
+++ src/api-sdks/rcs-api/clients/python/jupyter-notebooks/rcs-optim-example-v2/main.py
@@ -1,7 +1,15 @@
 import os
 from project_calculator_v2 import get_section_details
+from project_calculator_v2 import calc_rcs_crack_width
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 rcs_project_file_path = os.path.join(dir_path, 'crack-width-example.IdeaRcs')
 
 get_section_details(rcs_project_file_path)
+
+#The increment of moment increase after each step.
+moment_step = 3000
+
+section_in_rcs_project = 1
+calc_rcs_crack_width(rcs_project_file_path, section_in_rcs_project, moment_step)
+
