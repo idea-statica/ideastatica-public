7c981780e13f171eacb37a762fcb016f16bd359b
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-10-16 16:17:32 +0200

    REST API Calculator Clients v25.1.0.3974

--- src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
+++ src/api-sdks/connection-api/clients/python/ideastatica_connection_api/configuration.py
@@ -385,7 +385,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0\n"\
-               "SDK Package Version: 25.1.0.3672".\
+               "SDK Package Version: 25.1.0.3974".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
