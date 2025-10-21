135cf53e21a991969ad36171d42709fee8fc5ba1
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-10-21 16:05:50 +0200

    IS 25.1.0.4141

--- src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
+++ src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "25.1.0.3974"
+__version__ = "25.1.0.4141"
 
 # import apis into sdk package
 from ideastatica_rcs_api.api.calculation_api import CalculationApi
