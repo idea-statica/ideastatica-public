8ed64451186c552772e826fb61a209165c5c193e
Martin Pospisil <mpospisil@users.noreply.github.com>
2025-11-04 23:38:20 +0100

    Rcsapi loading (#1025)
    * fix
    
    * RcsApiWpfClientApp - load update example
    
    * clients v25.1.1.0755
    
    ---------
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

--- src/api-sdks/connection-api/clients/python/ideastatica_connection_api/__init__.py
+++ src/api-sdks/connection-api/clients/python/ideastatica_connection_api/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "25.1.0.4141"
+__version__ = "25.1.1.0755"
 
 # import apis into sdk package
 from ideastatica_connection_api.api.calculation_api import CalculationApi
