5894f02e5285954419d06090302926eb3e69ae30
Martin Pospisil <mpospisil@users.noreply.github.com>
2025-11-20 15:31:47 +0100

    Is v25.1.1 (#1030)
    * nuget update IS 25.1.1.923
    
    * clients IS 25.1.1.0923
    
    * updating PIP 25.1.1.923
    
    * Version = 25.1.0
    
    ---------
    
    Co-authored-by: Martin Pospisil <martin.pospisil@ideastatica.com>

--- src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
+++ src/api-sdks/rcs-api/clients/python/ideastatica_rcs_api/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "25.1.1.0755"
+__version__ = "25.1.1.0923"
 
 # import apis into sdk package
 from ideastatica_rcs_api.api.calculation_api import CalculationApi
