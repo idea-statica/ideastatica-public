c4897f18540c6db0a5e834db5bdf83c7820214a2
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-11-22 00:30:19 +0100

    Add new property MemberIds so user can specify members that gonna be used for partial template aplication

--- src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateMappingGetParam.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateMappingGetParam.cs
@@ -1,7 +1,10 @@
-﻿namespace IdeaStatiCa.Api.Connection.Model
+﻿using System.Collections.Generic;
+
+namespace IdeaStatiCa.Api.Connection.Model
 {
 	public class ConTemplateMappingGetParam
 	{
 		public string Template { get; set; }
+		public List<int> MemberIds { get; set; } = new List<int>();
 	}
 }
