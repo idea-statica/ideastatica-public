cff4d87585b7275eaf35005425c46ee83195fbbb
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-11-24 16:36:41 +0100

    Extend ConTemplateApplyResult with new properties so we do not return empty class which will not provide any inforations whether the application was succesfull. Create ConNonConformityIssue and ConNonConformityIssueSeverity that are used for that

---
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConNonConformityIssue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdeaStatiCa.Api.Connection.Model
+{
+	public class ConNonConformityIssue
+	{
+		/// <summary>
+		/// ID of the object related to this issue (if applicable)
+		/// </summary>
+		public int ObjectId { get; set; }
+		/// <summary>
+		/// Description of the issue (title)
+		/// </summary>
+		public string Description { get; set; }
+
+		/// <summary>
+		/// Detailed description of the issue
+		/// </summary>
+		public string Details { get; set; }
+
+		/// <summary>
+		/// Severity level: 0=Info, 1=Warning, 2=Error, 3=TerminatedError
+		/// </summary>
+		public ConNonConformityIssueSeverity Severity { get; set; }
+
+	}
+}
--- src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateApplyResult.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateApplyResult.cs
@@ -1,6 +1,23 @@
-﻿namespace IdeaStatiCa.Api.Connection.Model
+﻿using System.Collections.Generic;
+
+namespace IdeaStatiCa.Api.Connection.Model
 { 
 	public class ConTemplateApplyResult
 	{
+		/// <summary>
+		/// Indicates whether the template application succeeded without critical errors
+		/// </summary>
+		public bool IsSuccess { get; set; }
+
+		/// <summary>
+		/// List of issues encountered during template application
+		/// </summary>
+		public List<ConNonConformityIssue> Issues { get; set; }
+
+		public ConTemplateApplyResult()
+		{
+			Issues = new List<ConNonConformityIssue>();
+			IsSuccess = true;
+		}
 	}
 }
