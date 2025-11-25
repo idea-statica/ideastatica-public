7b1118eb073f78788a138d06cdc2d92c04071114
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-11-25 19:36:20 +0100

    Update ConConnectionTemplateModel to have all ids properties and ctor Add ConConnectionTemplateModel to ConTemplateApplyParam

--- src/IdeaStatiCa.Api/Connection/Model/Template/ConConnectionTemplateModel.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConConnectionTemplateModel.cs
@@ -5,12 +5,72 @@ namespace IdeaStatiCa.Api.Connection.Model
 {
 	public class ConConnectionTemplateModel
 	{
-		public string TemplateName { get; set; }
-		public Guid TempalteGuid { get; set; }
-		public Guid InstanceGuid { get; set; }
+		/// <summary>
+		/// Represents the unique identifier of the template
+		/// If the same template is used in multiple times, the same Guid is used.
+		/// </summary>
+		public Guid TempalteId { get; set; }
+
+		/// <summary>
+		/// Unique identifier of template within the connection instance
+		/// </summary>
+		public int InstanceId { get; set; }
+
+		/// <summary>
+		/// Ids of members used in template
+		/// </summary>
 		public List<int> MemberIds { get; set; } = new List<int>();
+
+		/// <summary>
+		/// Operation Ids used in template
+		/// </summary>
 		public List<int> OperationIds { get; set; } = new List<int>();
+
+		/// <summary>
+		/// Operation parameter Ids used in template
+		/// </summary>
 		public List<int> ParameterIds { get; set; } = new List<int>();
-		public List<int> CommonPropertyIds { get; set; } = new List<int>();
+
+		/// <summary>
+		/// Id of common plate material used in all operations of the template
+		/// If template do not use common plate material, the value is null
+		/// </summary>
+		public int? PlateMaterialId = null;
+
+		/// <summary>
+		/// Id of common weld material used in all operations of the template
+		/// If template do not use common weld material, the value is null
+		/// </summary>
+		public int? WeldMaterialId = null;
+
+		/// <summary>
+		/// Id of common bolt assembly used in all operations of the template
+		/// If template do not use common bolt assembly, the value is null
+		/// </summary>
+		public int? BoltAssemblyId = null;
+
+		public ConConnectionTemplateModel()
+		{
+			
+		}
+
+		public ConConnectionTemplateModel(Guid tempalteId, 
+			int instanceId,
+			List<int> memberIds, 
+			List<int> operationIds, 
+			List<int> parameterIds, 
+			int? plateMaterialId, 
+			int? weldMaterialId, 
+			int? boltAssemblyId)
+		{
+			TempalteId = tempalteId;
+			InstanceId = instanceId;
+			MemberIds = memberIds;
+			OperationIds = operationIds;
+			ParameterIds = parameterIds;
+			PlateMaterialId = plateMaterialId;
+			WeldMaterialId = weldMaterialId;
+			BoltAssemblyId = boltAssemblyId;
+		}
 	}
 }
--- src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateApplyResult.cs
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConTemplateApplyResult.cs
@@ -5,13 +5,21 @@ namespace IdeaStatiCa.Api.Connection.Model
 	public class ConTemplateApplyResult
 	{
 		/// <summary>
-		/// Indicates whether the template application succeeded without  any NonConformity issues or with just information level issues
+		/// Indicates whether the template application succeeded without any NonConformity issues or with just information level issues
 		/// </summary>
-		public bool IsSuccess { get; set; }
+		public bool AppliedWithoutIssues { get; set; }
+
+		/// <summary>
+		/// Model of the applied template
+		/// It contains Id of template and Ids of created members, operations, parameters, etc.
+		/// </summary>
+		public ConConnectionTemplateModel TemplateModel { get; set; }
 
 		/// <summary>
 		/// List of issues encountered during template application
 		/// </summary>
 		public List<ConNonConformityIssue> Issues { get; set; }
 	}
+
+
 }
