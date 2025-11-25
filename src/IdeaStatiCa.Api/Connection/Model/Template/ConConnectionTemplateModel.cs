bcad95369c3be6357cfa5b3a9e763d9fe9be2e2a
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2025-11-25 15:26:29 +0100

    Add ConConnectionTemplateModel

---
+++ src/IdeaStatiCa.Api/Connection/Model/Template/ConConnectionTemplateModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IdeaStatiCa.Api.Connection.Model
+{
+	public class ConConnectionTemplateModel
+	{
+		public string TemplateName { get; set; }
+		public Guid TempalteGuid { get; set; }
+		public Guid InstanceGuid { get; set; }
+		public List<int> MemberIds { get; set; } = new List<int>();
+		public List<int> OperationIds { get; set; } = new List<int>();
+		public List<int> ParameterIds { get; set; } = new List<int>();
+		public List<int> CommonPropertyIds { get; set; } = new List<int>();
+	}
+}
