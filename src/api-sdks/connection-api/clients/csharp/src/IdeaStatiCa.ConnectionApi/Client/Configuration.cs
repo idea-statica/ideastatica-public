1c9bf2aefe19623321513e9378624c74dcf467ca
Tomas Kohoutek <tomas.kohoutek@ideastatica.com>
2025-11-25 11:58:03 +0100

    #30552 generated clients

--- src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/Client/Configuration.cs
+++ src/api-sdks/connection-api/clients/csharp/src/IdeaStatiCa.ConnectionApi/Client/Configuration.cs
@@ -34,7 +34,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "25.1.1.0923";
+        public const string Version = "25.1.2.0791";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -118,7 +118,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
         public Configuration()
         {
             Proxy = null;
-            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/25.1.1.0923/csharp");
+            UserAgent = WebUtility.UrlEncode("OpenAPI-Generator/25.1.2.0791/csharp");
             BasePath = "http://localhost";
             DefaultHeaders = new ConcurrentDictionary<string, string>();
             ApiKey = new ConcurrentDictionary<string, string>();
@@ -541,7 +541,7 @@ namespace IdeaStatiCa.ConnectionApi.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 2.0\n";
-            report += "    SDK Package Version: 25.1.1.0923\n";
+            report += "    SDK Package Version: 25.1.2.0791\n";
 
             return report;
         }
