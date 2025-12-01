feafc76434d84aa0a55bd3deef1f0fe469425dff
Martin Pospisil <martin.pospisil@ideastatica.com>
2025-12-01 12:30:49 +0100

    TemplateIdentifier

---
+++ src/IdeaStatiCa.Api/Connection/Model/DesignProposer/TemplateIdentifier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace IdeaStatiCa.Api.Connection.Model.DesignProposer
+{
+	public class TemplateIdentifier
+	{
+		public Guid DesignSetId { get; set; }
+		public Guid DesignItemId { get; set; }
+	}
+}
