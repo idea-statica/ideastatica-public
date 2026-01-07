264721f2ca86d235feba7140020f5afb80fb3305
Martin Pospisil <martin.pospisil@ideastatica.com>
2026-01-07 23:47:32 +0100

    adding example ConnectionIomGenerator

---
+++ examples/iom/csharp/ConnectionIomGenerator/ConnectionIomGenerator/Model/ConnectionInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionIomGenerator.Model
+{
+	internal class ConnectionInput
+	{
+	}
+}
