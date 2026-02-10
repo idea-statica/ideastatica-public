fb7353470d02655e3711f3c4123dd906c67e5f02
Michal Vrana <michal.vrana@ideastatica.com>
2026-02-10 08:27:09 +0100

    #30690 - added new API for notification of parent apps about changes (replacement for old CurrentItemCHanged event)

---
+++ src/IdeaStatiCa.Plugin/Grpc/GrpcNotificationClient.cs
@@ -0,0 +1,108 @@
+﻿using Grpc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.Plugin.Grpc
+{
+	public interface IGrpcNotificationClient
+	{
+		void NotifyCurrentlyOpenedItemModified(int itemKey);
+		Task NotifyCurrentlyOpenedItemModifiedAsync(int itemKey);
+	}
+
+	/// <summary>
+	/// Client for gRPC notification service communication
+	/// </summary>
+	public class GrpcNotificationClient : IDisposable, IGrpcNotificationClient
+	{
+		private readonly GrpcNotificationService.GrpcNotificationServiceClient client;
+		private readonly Channel _channel;
+		private readonly IPluginLogger _logger;
+
+		/// <summary>
+		/// Creates gRPC notification service client
+		/// </summary>
+		/// <param name="logger">Logger</param>
+		/// <param name="port">Port</param>
+		/// <param name="host">Host</param>
+		/// <param name="maxDataLength">Maximum message length in bytes for channel</param>
+		public GrpcNotificationClient(IPluginLogger logger, int port, string host = "localhost", int maxDataLength = Constants.GRPC_MAX_MSG_SIZE)
+		{
+			this._logger = logger;
+			_channel = new Channel(host, port, ChannelCredentials.Insecure, CommunicationTools.GetChannelOptions(maxDataLength));
+			client = new GrpcNotificationService.GrpcNotificationServiceClient(_channel);
+
+			logger.LogDebug($"Created GrpcNotificationClient, host: '{host}', port: '{port}',  maxDataLength: '{maxDataLength}'");
+		}
+
+		/// <summary>
+		/// Notifies parent application, that currently opened item have been changed
+		/// </summary>
+		/// <param name="itemKey">Blob storage id</param>
+		/// <returns></returns>
+		public void NotifyCurrentlyOpenedItemModified(int itemKey)
+		{
+			_logger.LogDebug($"GrpcNotificationClient starts NotifyCurrentlyOpenedItemModified, itemKey: '{itemKey}'");
+
+			try
+			{
+				client.CurrentlyOpenedItemModified(new ItemNotifyRequest
+				{
+					ItemKey = itemKey
+				});
+
+				_logger.LogDebug($"GrpcNotificationClient ends NotifyCurrentlyOpenedItemModified, itemKey: '{itemKey}'");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogDebug("GrpcNotificationClient NotifyCurrentlyOpenedItemModified failed.", ex);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Notifies parent application, that currently opened item have been changed
+		/// </summary>
+		/// <param name="itemKey">Blob storage id</param>
+		/// <returns></returns>
+		public async Task NotifyCurrentlyOpenedItemModifiedAsync(int itemKey)
+		{
+			_logger.LogDebug($"GrpcNotificationClient starts NotifyCurrentlyOpenedItemModified, itemKey: '{itemKey}'");
+
+			try
+			{
+				await client.CurrentlyOpenedItemModifiedAsync(new ItemNotifyRequest
+				{
+					ItemKey = itemKey
+				});
+
+				_logger.LogDebug($"GrpcNotificationClient ends NotifyCurrentlyOpenedItemModified, itemKey: '{itemKey}'");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogDebug("GrpcNotificationClient NotifyCurrentlyOpenedItemModified failed.", ex);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Shuts down communication channel with gRPC server
+		/// </summary>
+		public void Dispose()
+		{
+			try
+			{
+				if (!_channel.ShutdownAsync().Wait(TimeSpan.FromMinutes(1)))
+				{
+					_logger.LogDebug("Time out - gRPC client does not shut down channel within one minute.");
+				}
+
+				_logger.LogDebug("Disposed GrpcNotificationClient");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogDebug("Dispose of GrpcNotificationClient failed.", ex);
+			}
+		}
+	}
+}
---
+++ src/IdeaStatiCa.Public/IParentAppNotificationHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace IdeaStatiCa.Public
+{
+	/// <summary>
+	/// Responsible for handling notification of changed in currently opened item
+	/// </summary>
+	public interface IParentAppNotificationHandler
+	{
+		/// <summary>
+		/// Notifies parent application, that currently opened item changed
+		/// </summary>
+		/// <param name="itemKey">identified of currently opened item</param>
+		Task NotifyItemChangedAsync(int itemKey);
+	}
+}
