624c244aa728b533b5589f8c6e8791e2c1d80e8a
stefano.kusuma@ideastatica.com <stefano8795@gmail.com>
2026-02-24 14:06:38 +0800

    first commit

---
+++ src/bim-links/IdeaStatiCa.Yjk/Main.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaStatiCa.Yjk
+{
+    public class Main
+    {
+		[CommandMethod("test_idea_plugin")]
+		public void Test()
+		{
+			MessageBox.Show("Import completed");
+		}
+	}
+}
