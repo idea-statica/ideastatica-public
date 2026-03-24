61ce0a70f39717fc38b328ef7d8bbebfccbecc51
Ondrej Zitko <ondrej.zitko@ideastatica.com>
2026-03-24 22:45:35 +0100

    Extend interface IConnectionAutomation with two new methods

--- src/IdeaStatiCa.Plugin/Api/Connection/IConnectionAutomation.cs
+++ src/IdeaStatiCa.Plugin/Api/Connection/IConnectionAutomation.cs
@@ -1,18 +1,31 @@
 ﻿using IdeaRS.OpenModel.Connection;
 using IdeaStatiCa.Public;
+using System.Threading.Tasks;
 
 
 namespace IdeaStatiCa.Plugin
 {
 	/// <summary>
-	/// 
+	///
 	/// </summary>
 	public interface IConnectionAutomation
 	{
 		/// <summary>
-		/// 
+		///
 		/// </summary>
 		/// <returns></returns>
 		ConProjectInfo GetProjectInfo();
+
+		/// <summary>
+		/// Returns whether the currently open connection has unsaved changes.
+		/// </summary>
+		bool GetIsEdited();
+
+		/// <summary>
+		/// Saves the currently open project without closing it.
+		/// Equivalent to clicking the Save button in the Connection application.
+		/// Does nothing if there is no open project.
+		/// </summary>
+		Task SaveCurrentItemAsync();
 	}
 }
